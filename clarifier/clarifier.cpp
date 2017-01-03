14ecb3cc2ca558b4241ec331b5ecbe1f12bd4a12
Reinaldo Molina <rmolin88@gmail.com>
2017-01-02 21:08:24 -0500

    many cool updates

--- clarifier/clarifier.cpp
+++ clarifier/clarifier.cpp
@@ -19,83 +19,82 @@ int main(int argc, char const *argv[])
 		}
 
 		ConfigFile configFile(argv[1]);
+		ConfigFile config();
 
-		libconfig::Config filecfg;
-		filecfg.readFile(argv[1]);
-		const libconfig::Setting &settRoot = filecfg.getRoot();
-		std::cout << "Main(): settRoot.getPath() = " << settRoot.getPath() << std::endl;
-		if (settRoot.getLength() < 1)
-		{
-			std::cerr << "Main(): No main options in the config file\n";
-			return -1;
-		}
+#ifndef NDEBUG
+		configFile.GetMainOptions();
+#endif
 
-		const libconfig::Setting &settMill = settRoot[OPT_MILLBALANCE];
-		int iNumOptions = settMill.getLength();
+		MaterialBalance mill_data;
 
-		try
-		{
-			for (int k=0; k<iNumOptions; k++)
-			{
-				const libconfig::Setting &settBuff = settMill[k];
-				std::string sPath = settBuff.getPath();
-				int iValue;
-				double dValue;
+		if ( (configFile.filecfg.lookupValue("MillMaterialBalance.GrindingRateTmCaneDay", mill_data.iGrindingRateTmCaneDay)) )
+			std::cout << mill_data.iGrindingRateTmCaneDay << std::endl;
+		// libconfig::Config filecfg;
+		// filecfg.readFile(argv[1]);
+		// const libconfig::Setting &settRoot = filecfg.getRoot();
+		// std::cout << "Main(): settRoot.getPath() = " << settRoot.getPath() << std::endl;
+		// if (settRoot.getLength() < 1)
+		// {
+			// std::cerr << "Main(): No main options in the config file\n";
+			// return -1;
+		// }
 
-				if (settBuff.getType() == libconfig::Setting::TypeInt)
-				{
-					if (filecfg.lookupValue(sPath, iValue))
-						std::cout << sPath << " = " << iValue << std::endl;
-					else
-						std::cout << "Couldnt decode " << sPath << std::endl;
-				}
-				else if (settBuff.getType() == libconfig::Setting::TypeFloat)
-				{
-					if (filecfg.lookupValue(sPath, dValue))
-						std::cout << sPath << " = " << dValue << std::endl;
-					else
-						std::cout << "Couldnt decode " << sPath << std::endl;
-				}
-				else if (settBuff.getType() == libconfig::Setting::TypeGroup)
-				{
-					int iNumOpt = settBuff.getLength();
-					for (int k1=0; k1 < iNumOpt; k1++)
-					{
-						const libconfig::Setting &settBuff1 = settBuff[k1];
-						sPath = settBuff1.getPath();
-						if (filecfg.lookupValue(sPath, dValue))
-							std::cout << sPath << " = " << dValue << std::endl;
-						else
-							std::cout << "Couldnt decode " << sPath << std::endl;
-					}
-				}
-				else
-				{
-					std::cout << "Setting " << sPath << " is of unexpected type" << std::endl;
-					return -1;
-				}
-			}
-		}
-		catch(const libconfig::SettingNotFoundException &nfex)
-		{
-			std::cout << "Setting not found\n";
-		}
+		// const libconfig::Setting &settMill = settRoot[OPT_MILLBALANCE];
+		// int iNumOptions = settMill.getLength();
+
+		// try
+		// {
+			// for (int k=0; k<iNumOptions; k++)
+			// {
+				// const libconfig::Setting &settBuff = settMill[k];
+				// std::string sPath = settBuff.getPath();
+				// int iValue;
+				// double dValue;
+
+				// if (settBuff.getType() == libconfig::Setting::TypeInt)
+				// {
+					// if (filecfg.lookupValue(sPath, iValue))
+						// std::cout << sPath << " = " << iValue << std::endl;
+					// else
+						// std::cout << "Couldnt decode " << sPath << std::endl;
+				// }
+				// else if (settBuff.getType() == libconfig::Setting::TypeFloat)
+				// {
+					// if (filecfg.lookupValue(sPath, dValue))
+						// std::cout << sPath << " = " << dValue << std::endl;
+					// else
+						// std::cout << "Couldnt decode " << sPath << std::endl;
+				// }
+				// else if (settBuff.getType() == libconfig::Setting::TypeGroup)
+				// {
+					// int iNumOpt = settBuff.getLength();
+					// for (int k1=0; k1 < iNumOpt; k1++)
+					// {
+						// const libconfig::Setting &settBuff1 = settBuff[k1];
+						// sPath = settBuff1.getPath();
+						// if (filecfg.lookupValue(sPath, dValue))
+							// std::cout << sPath << " = " << dValue << std::endl;
+						// else
+							// std::cout << "Couldnt decode " << sPath << std::endl;
+					// }
+				// }
+				// else
+				// {
+					// std::cout << "Setting " << sPath << " is of unexpected type" << std::endl;
+					// return -1;
+				// }
+			// }
+		// }
+		// catch(const libconfig::SettingNotFoundException &nfex)
+		// {
+			// std::cout << "Setting not found\n";
+		// }
 		std::cout << "Hello, World!\n";
 		return 0;
 	}
-	catch (const libconfig::FileIOException &fioex)
-	{
-		std::cerr << "I/O error while reading file.\n";
-		return (EXIT_FAILURE);
-	}
-	catch (const libconfig::ParseException &pex)
-	{
-		std::cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine() << " - " << pex.getError() << std::endl;
-		return (EXIT_FAILURE);
-	}
 	catch (...)
 	{
-		std::cerr << "Main(): Unkown exception happened\n";
+		std::cerr << "Main(): Exception\n";
 	}
 }
 
