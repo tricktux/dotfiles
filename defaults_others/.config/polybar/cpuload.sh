dbe9afdfa22e2444a1f216d8626bef01b4eae4fa
Reinaldo Molina <rmolin88@gmail.com>
2017-11-19 20:29:40 -0500

    polybar: updates

--- defaults_others/.config/polybar/cpuload.sh
+++ defaults_others/.config/polybar/cpuload.sh
@@ -2,5 +2,8 @@
 
 # Note: Taken from here:
 # https://github.com/jaagr/polybar/wiki/User-contributed-modules#cpu-load
+# echo "$(uptime -p)" "$(uptime | grep -ohe 'load average[s:][: ].*' | sed 's/,// g')"
+
 
 uptime | grep -ohe 'load average[s:][: ].*' | sed 's/,//g' | awk '{print $3" "$4" "$5}'
+
