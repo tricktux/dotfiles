3e8e066b2597689531dc0a4535e839eea98e2294
Reinaldo Molina <rmolin88@gmail.com>
2018-02-23 06:12:11 -0500

    More ways to bleach and no trailing spaces

--- scripts/arch-maintance.sh
+++ scripts/arch-maintance.sh
@@ -4,7 +4,7 @@
 # Description:			File to give arch maintance
 # Author:				Reinaldo Molina <rmolin88@gmail.com>
 # Version:				0.0.0
-# Last Modified: Oct 15 2017 10:03
+# Last Modified: Fri Feb 23 2018 06:11
 # Created: Oct 15 2017 10:03
 
 machine=`hostname`
@@ -33,7 +33,7 @@ sudo pacman -Qmq > ~/.config/dotfiles/$machine.aur
 
 echo "BleachBit runnning"
 
-# This options will use what is set in the ~/.config/bleachbit/bleachbit.ini 
+# This options will use what is set in the ~/.config/bleachbit/bleachbit.ini
 # The options there come from what is set up in the gui.
 # PLEASE: open the gui and make sure that you have the right options there
 # Also consider adding this options
@@ -45,6 +45,8 @@ echo "BleachBit runnning"
 # Also disable system.memory clean swap but requires sudo
 
 bleachbit --clean --preset
+# Alternative
+# bleachbit --list | grep -E "[a-z0-9_\-]+\.[a-z0-9_\-]+" | grep -v system.free_disk_space | xargs bleachbit --clean
 
 # echo "Cleaning shitty files"
 # curl -kLo ~/.cache/rmshit.py "https://raw.githubusercontent.com/lahwaacz/Scripts/master/rmshit.py"
