060ee4a4b51c9ac299951671297986b28c60a59b
Reinaldo Molina <rmolin88@gmail.com>
2019-01-17 21:14:21 -0500

    New script to make it easy substituting journal applications

---
+++ defaults/.config/polybar/scripts/journal.sh
@@ -0,0 +1,9 @@
+# File:           journal.sh
+# Description:    Launch editor for journal
+# Author:		    Reinaldo Molina
+# Email:          rmolin88 at gmail dot com
+# Revision:	    0.0.0
+# Created:        Thu Jan 17 2019 21:07
+# Last Modified:  Thu Jan 17 2019 21:07
+
+vim --servername /tmp/nada
