d2b2ccdaf7ebb51649786ebd8db873a066c86dc4
Reinaldo Molina <rmolin88@gmail.com>
2019-01-19 18:20:17 -0500

    Cleaning up

--- defaults/.config/polybar/scripts/launch.sh
+++ defaults/.config/polybar/scripts/launch.sh
@@ -7,11 +7,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-# if [[ $machine = "predator" || $machine = "predator" ]]; then
-	# polybar --reload top-$machine&
-# else
-	polybar --reload top-$machine&
-# fi
-
+polybar --reload top-$machine&
 polybar --reload bottom-$machine&
