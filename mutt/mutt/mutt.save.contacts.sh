bfffb6f582ca097a35ab1e5c39ee2967cba0c8c1
Reinaldo Molina <rmolin88@gmail.com>
2019-01-21 00:26:49 -0500

    Fixed a long time issue that cause bad screen drawing

--- mutt/mutt/mutt.save.contacts.sh
+++ mutt/mutt/mutt.save.contacts.sh
@@ -4,7 +4,7 @@ MESSAGE=$(cat)
 
 NEWALIAS=$(echo "${MESSAGE}" | grep ^"From: " | sed s/[\,\"\']//g | awk '{$1=""; if (NF == 3) {print "alias" $0;} else if (NF == 2) {print "alias" $0 $0;} else if (NF > 3) {print "alias", tolower($(NF-1))"-"tolower($2) $0;}}')
 
-if grep -Fxq "$NEWALIAS" $HOME/local/share/mutt/aliases.txt; then
+if grep -Fxq "$NEWALIAS" $HOME/.local/share/mutt/aliases.txt; then
 	:
 else
 	echo "$NEWALIAS" >> $HOME/.local/share/mutt/aliases.txt
