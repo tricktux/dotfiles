717dade464d8db7d7a1b2b107bf3d1ed88d28dae
Reinaldo Molina <rmolin88@gmail.com>
2019-01-23 07:14:15 -0500

    Improve readability and functionality

--- neomutt/mutt.save.contacts.sh
+++ neomutt/mutt.save.contacts.sh
@@ -2,12 +2,20 @@
 
 MESSAGE=$(cat)
 
-NEWALIAS=$(echo "${MESSAGE}" | grep ^"From: " | sed s/[\,\"\']//g | awk '{$1=""; if (NF == 3) {print "alias" $0;} else if (NF == 2) {print "alias" $0 $0;} else if (NF > 3) {print "alias", tolower($(NF-1))"-"tolower($2) $0;}}')
+alias_loc = $HOME/.config/neomutt/data/aliases.txt
 
-if grep -Fxq "$NEWALIAS" $HOME/.local/share/mutt/aliases.txt; then
+NEWALIAS=$(echo "${MESSAGE}" \
+	grep ^"From: " \
+	sed s/[\,\"\']//g \
+	awk '{$1=""; if (NF == 3) {print "alias" $0;} else if (NF == 2) {print "alias" $0 $0;} else if (NF > 3) {print "alias", tolower($(NF-1))"-"tolower($2) $0;}}')
+
+# Create file if it doesnt exist
+[ -f /etc/hosts ] || touch $alias_loc
+
+if grep -Fxq "$NEWALIAS" $alias_loc; then
 	:
 else
-	echo "$NEWALIAS" >> $HOME/.local/share/mutt/aliases.txt
+	echo "$NEWALIAS" >> $alias_loc
 fi
 
 echo "${MESSAGE}"
