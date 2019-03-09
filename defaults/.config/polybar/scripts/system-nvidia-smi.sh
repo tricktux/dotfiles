a0022d2824b3eb2b5ea280c1e14c77b8ee7b7cd7
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-03-09 06:57:23 -0500

    Enforce tw 80

--- defaults/.config/polybar/scripts/system-nvidia-smi.sh
+++ defaults/.config/polybar/scripts/system-nvidia-smi.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{ print "GPU",""$1"","%"}'
+nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | \
+	awk '{ print "GPU",""$1"","%"}'
