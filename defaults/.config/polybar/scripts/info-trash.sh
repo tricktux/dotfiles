8400ff5e00b5b2447f8c1b6860260ca3c12efc22
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-04-17 22:27:24 -0400

    No option parsing

--- defaults/.config/polybar/scripts/info-trash.sh
+++ defaults/.config/polybar/scripts/info-trash.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
-case "$1" in
-    --clean)
-		termite -e ~/.config/dotfiles/scripts/arch-maintance.sh
-        ;;
-    *)
-		num_trash=$(ls -la ~/.local/share/Trash/files | wc -l)
-		if [ "$num_trash"  -gt 30 ]; then
-			echo "$num_trash "
-		else
-			echo ""
-		fi
-        ;;
-esac
+num_trash=$(ls -la ~/.local/share/Trash/files | wc -l)
+if [ "$num_trash"  -gt 30 ]; then
+	echo "$num_trash "
+else
+	echo ""
+fi
