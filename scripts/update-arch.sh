4a35d0b5b039b5e9b352415665d84f20e8225b84
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-04-17 22:48:32 -0400

    Add zplug file. works just like an udate

--- scripts/update-arch.sh
+++ scripts/update-arch.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+i3-msg "floating toggle"
+
 echo "Updating pacman mirrors ..."
 if hash reflector 2>/dev/null; then
 	sudo reflector --protocol https --latest 30 --number 5 --sort \
@@ -18,7 +20,9 @@ echo "Updating neovim ..."
 trizen -S neovim-git
 nvim +PlugUpgrade +PlugUpdate +UpdateRemotePlugins
 
-echo "Updating zplug ..."
-zplug update
+# Wed Apr 17 2019 22:46
+# Gets updated on arch-maintance.sh script
+# echo "Updating zplug ..."
+# zplug update
 
 read -n1 -r -p "Done. Press any key to continue..." key
