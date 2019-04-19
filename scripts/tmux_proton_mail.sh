4e6c370ff7d309c708c2786be6cb6e50e26959e0
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-04-19 09:38:03 -0400

    Working on the new tmux setup

--- scripts/tmux_proton_mail.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-tmux new-session \; \
-	send-keys 'protonmail-bridge --cli' C-m \; \
-	split-window -h \; \
-	send-keys 'sleep 1 && neomutt -F ~/.config/neomutt/user.pm' C-m \; \
-	resize-pane -Z \; 
