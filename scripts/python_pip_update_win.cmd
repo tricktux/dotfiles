5a68b0a63fdae8e911f745976aeb3d5c3c1b38a9
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-05-23 12:02:03 -0400

    Analogous to unix script that updates all pip stuff

---
+++ scripts/python_pip_update_win.cmd
@@ -0,0 +1,24 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+set modules[0]=vim-vint
+set modules[1]=thesaurus
+set modules[2]=neovim-remote
+set modules[3]=psutil
+set modules[4]=flake8
+set modules[5]=isort
+set modules[6]=jedi
+set modules[7]=pynvim
+set modules[8]=python-language-server
+set modules[9]=pip
+set modules[10]=frosted
+set modules[11]=pep8
+set modules[12]=pylint
+
+REM  set modules_2[0]=${modules[7]}
+REM  set modules_2[1]=${modules[9]}
+REM
+for /l %%n in (0,1,12) do ( 
+	pip install --upgrade --user !modules[%%n]! 
+)
