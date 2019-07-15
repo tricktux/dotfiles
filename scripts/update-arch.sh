2ab717f423749dce3b8fbef9f857754ba08e2fd9
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-07-14 21:06:29 -0400

    Workaround to install pandoc stuff
    Also be more specific with reflector

--- scripts/update-arch.sh
+++ scripts/update-arch.sh
@@ -3,7 +3,7 @@
 i3-msg "floating toggle"
 
 echo "Updating pacman mirrors ..."
-if hash reflector 2>/dev/null; then
+if hash /usr/bin/reflector 2>/dev/null; then
 	sudo reflector --protocol https --latest 30 --number 5 --sort \
 		rate --save /etc/pacman.d/mirrorlist -c 'United States' --verbose
 else
@@ -26,5 +26,14 @@ nvim +PlugUpgrade +PlugUpdate +UpdateRemotePlugins
 # Gets updated on arch-maintance.sh script
 # echo "Updating zplug ..."
 # zplug update
+
+# Sun Jul 14 2019 20:59
+# For some reason pandoc-{citeproc,crossref}-bin have been deleted from aur
+# So use this temporary workaround
+echo "Updating pandoc binaries..."
+cd /tmp
+git clone --recursive https://github.com/ashwinvis/aur.git
+cd aur
+makepkg -si
 
 read -n1 -r -p "Done. Press any key to continue..." key
