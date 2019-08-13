d87978a0fef1dbcbc04bf5e9afc029ce7793799b
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-08-12 21:16:04 -0400

    Formatting

--- scripts/rm_junk.py
+++ scripts/rm_junk.py
@@ -3,66 +3,66 @@ import shutil
 import glob
 
 shittyfiles = [
-        '~/.cache/',
-        '~/.local/share/qutebrowser/',
-        '~/.local/share/Trash/files/',
-        '~/.local/share/Trash/info/',
-        '~/.python_history',
-        '~/.zplug/',
-        '~/.zcompdump*',
-        '~/.local/share/xorg/'
-        '~/.adobe',              # Flash crap
-        '~/.macromedia',         # Flash crap
-        '~/.recently-used',
-        '~/.local/share/recently-used.xbel',
-        '~/Desktop',             # Firefox creates this
-        '~/.thumbnails',
-        '~/.gconfd',
-        '~/.gconf',
-        '~/.local/share/gegl-0.2',
-        '~/.FRD/log/app.log',   # FRD
-        '~/.FRD/links.txt',     # FRD
-        '~/.objectdb',          # FRD
-        '~/.gstreamer-0.10',
-        '~/.pulse',
-        '~/.pulse-cookie',
-        '~/.esd_auth',
-        '~/.config/enchant',
-        '~/.spicec',            # contains only log file; unconfigurable
-        '~/.dropbox-dist',
-        '~/.parallel',
-        '~/.dbus',
-        '~/ca2',                # WTF?
-        '~/ca2~',               # WTF?
-        '~/.distlib/',          # contains another empty dir, don't know which software creates it
-        '~/.bazaar/',           # bzr insists on creating files holding default values
-        '~/.bzr.log',
-        '~/.nv/',
-        '~/.viminfo',           # configured to be moved to ~/.cache/vim/viminfo, but it is still sometimes created...
-        '~/.npm/',              # npm cache
-        '~/.java/',
-        '~/.oracle_jre_usage/',
-        '~/.jssc/',
-        '~/.lsp4xml/',
-        '~/.tooling/',
-        '~/.webclipse/',
-        '~/.swt/',
-        '~/.sonar/',
-        '~/.quake2rtx/',
-        '~/.tox/',              # cache directory for tox
-        '~/.pylint.d/',
-        '~/.qute_test/',
-        '~/.QtWebEngineProcess/',
-        '~/.qutebrowser/',      # created empty, only with webengine backend
-        '~/.asy/',
-        '~/.cmake/',
-        '~/.gnome/',
-        '~/unison.log',
-        '~/.texlive/',
-        '~/.w3m/',
-        '~/.subversion/',
-        '~/nvvp_workspace/',    # created empty even when the path is set differently in nvvp
-        ]
+    '~/.cache/',
+    '~/.local/share/qutebrowser/',
+    '~/.local/share/Trash/files/',
+    '~/.local/share/Trash/info/',
+    '~/.python_history',
+    '~/.zplug/',
+    '~/.zcompdump*',
+    '~/.local/share/xorg/'
+    '~/.adobe',  # Flash crap
+    '~/.macromedia',  # Flash crap
+    '~/.recently-used',
+    '~/.local/share/recently-used.xbel',
+    '~/Desktop',  # Firefox creates this
+    '~/.thumbnails',
+    '~/.gconfd',
+    '~/.gconf',
+    '~/.local/share/gegl-0.2',
+    '~/.FRD/log/app.log',  # FRD
+    '~/.FRD/links.txt',  # FRD
+    '~/.objectdb',  # FRD
+    '~/.gstreamer-0.10',
+    '~/.pulse',
+    '~/.pulse-cookie',
+    '~/.esd_auth',
+    '~/.config/enchant',
+    '~/.spicec',  # contains only log file; unconfigurable
+    '~/.dropbox-dist',
+    '~/.parallel',
+    '~/.dbus',
+    '~/ca2',  # WTF?
+    '~/ca2~',  # WTF?
+    '~/.distlib/',  # contains another empty dir, don't know which software creates it
+    '~/.bazaar/',  # bzr insists on creating files holding default values
+    '~/.bzr.log',
+    '~/.nv/',
+    '~/.viminfo',  # configured to be moved to ~/.cache/vim/viminfo, but it is still sometimes created...
+    '~/.npm/',  # npm cache
+    '~/.java/',
+    '~/.oracle_jre_usage/',
+    '~/.jssc/',
+    '~/.lsp4xml/',
+    '~/.tooling/',
+    '~/.webclipse/',
+    '~/.swt/',
+    '~/.sonar/',
+    '~/.quake2rtx/',
+    '~/.tox/',  # cache directory for tox
+    '~/.pylint.d/',
+    '~/.qute_test/',
+    '~/.QtWebEngineProcess/',
+    '~/.qutebrowser/',  # created empty, only with webengine backend
+    '~/.asy/',
+    '~/.cmake/',
+    '~/.gnome/',
+    '~/unison.log',
+    '~/.texlive/',
+    '~/.w3m/',
+    '~/.subversion/',
+    '~/nvvp_workspace/',  # created empty even when the path is set differently in nvvp
+]
 
 
 def yesno(question, default="n"):
