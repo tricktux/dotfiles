74409ffe6919ab7bb0e67fbd4ab02b3fb9a04def
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-08-14 11:08:22 -0400

    MAJOR: re org of scripts folder

---
+++ scripts/win/vimrc_win_setup.bat
@@ -0,0 +1,5 @@
+:: Requiers admin cmd
+cd %HOME%
+git clone https://github.com/rmolin88/vimrc
+mklink _vimrc vimrc/_vimrc
+mklink .gitconfig vimrc/.gitconfig
---
+++ scripts/win/win_mklink.cmd
@@ -0,0 +1,5 @@
+mklink %USERPROFILE%\_vimrc %APPDATA%\dotfiles\defaults\.config\nvim\init.vim
+mklink %USERPROFILE%\_gvimrc %APPDATA%\dotfiles\defaults\.config\nvim\ginit.vim
+mklink %USERPROFILE%\.gitconfig %APPDATA%\dotfiles\.gitconfig_win
+mklink %USERPROFILE%\.gitignore_global %APPDATA%\dotfiles\defaults\.gitignore_global
+mklink "%APPDATA%\microsoft\windows\start menu\programs\startup\cleantemp.bat" %APPDATA%\dotfiles\defaults\scripts\cleantemp.bat
