6ccdb4df3e2eb9e5f27cac3612c76ad822e4aea7
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-08-14 11:08:22 -0400

    MAJOR: re org of scripts folder

---
+++ defaults/.config/i3/scripts/python_neovim_virtualenv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# File:           python_neovim_virtualenv.sh
+# Description:    Create virtual environment used by neovim
+# Author:		    Reinaldo Molina
+# Email:          rmolin88 at gmail dot com
+# Revision:	    0.0.0
+# Created:        Fri Jan 25 2019 23:09
+# Last Modified:  Fri Jan 25 2019 23:09
+
+modules[0]="vim-vint"
+modules[1]="thesaurus"
+modules[2]="neovim-remote"
+modules[3]="psutil"
+modules[4]="flake8"
+modules[5]="isort"
+modules[6]="jedi"
+modules[7]="pynvim"
+modules[8]="python-language-server"
+modules[9]="pip"
+modules[10]="frosted"
+modules[11]="pep8"
+modules[12]="pylint"
+modules[13]="requests"
+
+modules_2[0]=${modules[7]}
+modules_2[1]=${modules[9]}
+
+# mkdir -p $venv_loc
+# python -m venv $venv_loc/$venv_name
+# source $venv_name/bin/activate
+pip3 install --upgrade --user ${modules[*]}
+pip2 install --upgrade --user ${modules_2[*]}
+# deactivate
