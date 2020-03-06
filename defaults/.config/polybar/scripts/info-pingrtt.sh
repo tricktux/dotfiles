11322abd4685df90039d05e46c9d2dc17cde07a9
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-03-05 21:16:12 -0500

    Fixed ping script

--- defaults/.config/polybar/scripts/info-pingrtt.sh
+++ defaults/.config/polybar/scripts/info-pingrtt.sh
@@ -2,17 +2,18 @@
 
 HOST=1.1.1.1
 
-ping -n -c 1 -W $HOST > /dev/null 2>&1 || exit
-
-ping=ping -n -c 1 -W $HOST
-rtt=$(echo "$ping" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
-
-if [ "$rtt" -lt 50 ]; then
-	icon="%{F#3cb703}%{F-}"
-elif [ "$rtt" -lt 150 ]; then
-	icon="%{F#f9dd04}%{F-}"
+if ! ping=$(ping -n -c 1 -W 1 $HOST); then
+    echo "no %{F#d60606}%{F-}"
 else
-	icon="%{F#d60606}%{F-}"
-fi
+    rtt=$(echo "$ping" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
 
-echo " $icon $rtt ms "
+    if [ "$rtt" -lt 50 ]; then
+        icon="%{F#3cb703}%{F-}"
+    elif [ "$rtt" -lt 150 ]; then
+        icon="%{F#f9dd04}%{F-}"
+    else
+        icon="%{F#d60606}%{F-}"
+    fi
+
+    echo "$icon $rtt ms"
+fi
