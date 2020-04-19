58e46723cf7be25d492c79a3423acc1cb7e0436a
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-04-19 15:29:53 -0400

    Enforce tw 80

--- defaults/.config/tmux/neomutt.sh
+++ defaults/.config/tmux/neomutt.sh
@@ -10,11 +10,15 @@ session="neomutt"
 # Start the tmux server, if not already running, without creating any sessions.
 /usr/bin/tmux start-server
 
-/usr/bin/tmux new-session -d -s $session -n 'mailserver' 'neomutt -F ~/.config/neomutt/user.molinamail'
-/usr/bin/tmux new-window -d -t $session -n 'gmail' 'neomutt -F ~/.config/neomutt/user.gmail'
-/usr/bin/tmux new-window -d -t $session -n 'ufl' 'neomutt -F ~/.config/neomutt/user.ufl'
+/usr/bin/tmux new-session -d -s $session -n 'mailserver' \
+  'neomutt -F ~/.config/neomutt/user.molinamail'
+/usr/bin/tmux new-window -d -t $session -n 'gmail' \
+  'neomutt -F ~/.config/neomutt/user.gmail'
+/usr/bin/tmux new-window -d -t $session -n 'ufl' \
+  'neomutt -F ~/.config/neomutt/user.ufl'
 # Wed Feb 19 2020 20:11: Account has been disabled 
 # /usr/bin/tmux  new-window  -d -t $session -n 'psu' 'neomutt -F ~/.config/neomutt/user.psu'
 /usr/bin/tmux new-window -d -t $session -n 'pmserver' 'protonmail-bridge --cli'
-/usr/bin/tmux new-window -d -t $session -n 'pm' 'sleep 5; neomutt -F ~/.config/neomutt/user.pm'
+/usr/bin/tmux new-window -d -t $session -n 'pm' \
+  'sleep 5; neomutt -F ~/.config/neomutt/user.pm'
 /usr/bin/tmux -2 attach-session -t $session
