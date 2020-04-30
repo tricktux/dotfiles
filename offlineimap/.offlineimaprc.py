9c75bbc3fa6ad1f52e6065eae7a3c46d09cfb310
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-04-30 05:59:55 -0400

    Added gmail

--- offlineimap/.offlineimaprc.py
+++ offlineimap/.offlineimaprc.py
@@ -4,3 +4,6 @@ from subprocess import check_output
 
 def get_molinamail_pass():
     return check_output("pass linux/mailserver/me@molinamail.com", shell=True).splitlines()[0]
+
+def get_gmail_pass():
+    return check_output("pass websites/google.com/rmolin88/neomutt", shell=True).splitlines()[0]
