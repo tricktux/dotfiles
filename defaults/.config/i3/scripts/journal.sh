6fd5a662277527f67810d99cc75b2093d74d1177
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-06-15 21:55:35 -0400

    Use full paths

--- defaults/.config/i3/scripts/journal.sh
+++ defaults/.config/i3/scripts/journal.sh
@@ -6,5 +6,5 @@
 # Created:        Thu Jan 17 2019 21:07
 # Last Modified:  Thu Jan 17 2019 21:07
 
-nvim +UtilsEditJournal --listen /tmp/neovim_journal -i \
+/usr/bin/nvim +UtilsEditJournal --listen /tmp/neovim_journal -i \
 	~/.local/share/nvim/shada/journal.shada
