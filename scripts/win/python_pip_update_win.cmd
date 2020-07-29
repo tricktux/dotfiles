9e7789dd65cf4da0bc1bb80ef5760e0d3221855c
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-07-29 07:05:34 -0400

    Fixing modules and warnings in pip

--- scripts/win/python_pip_update_win.cmd
+++ scripts/win/python_pip_update_win.cmd
@@ -2,25 +2,25 @@
 
 setlocal enabledelayedexpansion
 
+REM  set modules[4]=isort It's already installed by pyling
 set modules[0]=vim-vint
-set modules[1]=thesaurus
-set modules[2]=neovim-remote
-set modules[3]=psutil
-set modules[4]=flake8
-set modules[5]=isort
-set modules[6]=jedi
-set modules[7]=pynvim
-set modules[8]=python-language-server
-set modules[9]=pip
-set modules[10]=frosted
-set modules[11]=pylint
-set modules[12]=neovim
-set modules[13]=yapf
-set modules[14]=docformatter
+set modules[1]=neovim-remote
+set modules[2]=psutil
+set modules[3]=flake8
+set modules[4]=jedi
+set modules[5]=pynvim
+set modules[6]=python-language-server
+set modules[7]=pip
+set modules[8]=frosted
+set modules[9]=pylint
+set modules[10]=neovim
+set modules[11]=yapf
+set modules[12]=docformatter
+set modules[13]=pylama
 
 REM  set modules_2[0]=${modules[7]}
 REM  set modules_2[1]=${modules[9]}
 REM
 for /l %%n in (0,1,13) do ( 
-	pip install --upgrade !modules[%%n]!
+    pip install --upgrade --use-feature=2020-resolver !modules[%%n]!
 )
