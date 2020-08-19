51ae513d101ae340b77ff2a8d96b894f11791b42
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-08-19 11:55:57 -0400

    Adjusting python modules in win
    Removed vint, not really working in windows anymore
    Also neovim-remote, doesnt work in win
    Adding ptpython, a better repl

--- scripts/win/python_pip_update_win.cmd
+++ scripts/win/python_pip_update_win.cmd
@@ -3,24 +3,20 @@
 setlocal enabledelayedexpansion
 
 REM  set modules[4]=isort It's already installed by pyling
-set modules[0]=vim-vint
-set modules[1]=neovim-remote
-set modules[2]=psutil
-set modules[3]=flake8
-set modules[4]=jedi
-set modules[5]=pynvim
-set modules[6]=python-language-server
-set modules[7]=pip
-set modules[8]=frosted
-set modules[9]=pylint
-set modules[10]=neovim
-set modules[11]=yapf
-set modules[12]=docformatter
-set modules[13]=pylama
+set modules[0]=psutil
+set modules[1]=flake8
+set modules[2]=jedi
+set modules[3]=pynvim
+set modules[4]=python-language-server
+set modules[5]=pip
+set modules[6]=frosted
+set modules[7]=pylint
+set modules[8]=neovim
+set modules[9]=yapf
+set modules[10]=docformatter
+set modules[11]=pylama
+set modules[12]=ptpython
 
-REM  set modules_2[0]=${modules[7]}
-REM  set modules_2[1]=${modules[9]}
-REM
-for /l %%n in (0,1,13) do ( 
-    pip install --upgrade --use-feature=2020-resolver !modules[%%n]!
+for /l %%n in (0,1,12) do ( 
+    pip install --upgrade !modules[%%n]!
 )
