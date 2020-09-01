3daff7b0658316c2609bfd2f4c25a0a908f60afd
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-09-01 16:20:43 -0400

    Pretty good setup so far

--- defaults/.config/nvim/lua/config/completion.lua
+++ defaults/.config/nvim/lua/config/completion.lua
@@ -1,27 +1,24 @@
-local function lsp_enable()
-    vim.cmd([[Plug 'neovim/nvim-lspconfig']])
+local CompletionNvim = {}
+local utils = require('utils/utils')
+local log = require('utils/log')
 
-    -- Internal variable
-    vim.g.nvim_lsp_support = 1
-end
-
-local function set()
-    vim.cmd([[Plug 'haorenW1025/completion-nvim']])
-    -- External source
-    vim.cmd([[Plug 'steelsojka/completion-buffers']])
-
-    vim.g.completion_enable_snippet = 'Neosnippet'
-    vim.g.completion_enable_in_comment = 1
-    vim.g.completion_trigger_keyword_length = 2
-    -- vim.g.completion_confirm_key = [[<c-k>]]
-    vim.g.completion_auto_change_source = 0
-    vim.g.completion_matching_ignore_case = 1
-    vim.g.completion_enable_auto_paren = 1
-    vim.g.completion_sorting = 'none'
-    vim.g.completion_matching_strategy_list = {'exact', 'fuzzy'}
-    vim.g.completion_sorting = 'none'
-    vim.g.completion_chain_complete_list =
-    {
+-- undocumented options:
+--  docked_maximum_size
+--  docked_minimum_size
+--  enable_focusable_hover
+CompletionNvim._opts = {
+    enable_snippet = 'Neosnippet',
+    enable_in_comment = 1,
+    trigger_keyword_length = 2,
+    auto_change_source = 0,
+    matching_ignore_case = 0,
+    enable_auto_paren = 0,
+    docked_hover = 1,
+    sorting = 'none',
+    matching_strategy_list = {'exact', 'fuzzy'},
+    sorting = 'none',
+    trigger_character = {'.'},
+    chain_complete_list = {
         default = {
             {complete_items = {'lsp', 'buffers', 'snippet'}},
             {complete_items = {'path'}, triggered_only = {'/'}},
@@ -31,8 +28,36 @@ local function set()
         },
         comment = {},
     }
+}
 
-    lsp_enable()
+CompletionNvim._autocmds = {
+    compl_nvim = {
+        {"BufEnter", "*", [[lua require('config/completion'):on_attach()]]};
+    }
+}
+
+function CompletionNvim:on_attach()
+    local ft = vim.bo.filetype
+    if ft == 'c' or ft == 'cpp' then
+        self._opts.trigger_character = {'.', '::', '->'}
+    elseif ft == 'lua' then
+        self._opts.trigger_character = {'.', ':'}
+    else
+        self._opts.trigger_character = {'.'}
+    end
+    -- TODO Add per buffer options
+    -- require'diagnostic'.on_attach()
+    require('completion').on_attach(self._opts)
 end
 
-return {set = set}
+function CompletionNvim:set()
+    if not utils.is_mod_available('completion') then
+        log.error("completion-nvim was set, but module not found")
+        return
+    end
+
+    log.info("setting up completion-nvim...")
+    utils.create_augroups(self._autocmds)
+end
+
+return CompletionNvim
