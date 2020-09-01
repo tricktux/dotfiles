9aede2dc9141029856601bb852c0377f36379e37
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-09-01 17:53:57 -0400

    Adding missing modules and better messages

--- defaults/.config/nvim/lua/config/lsp.lua
+++ defaults/.config/nvim/lua/config/lsp.lua
@@ -1,3 +1,6 @@
+local utils = require('utils/utils')
+local log = require('utils/log')
+
 -- TODO
 -- Maybe set each server to its own function?
 -- What about completion-nvim on_attach
@@ -8,24 +11,24 @@ local function lsp_set()
   end
 
   local nvim_lsp = require'nvim_lsp'
-
-  if vim.fn.executable('pyls') then
-    log.info("setting up pyls...")
+  if vim.fn.executable('pyls') > 0 then
+    log.info("setting up the pyls lsp...")
     nvim_lsp.pyls.setup {
       cmd = {"pyls"},
       root_dir = nvim_lsp.util.root_pattern(".git", ".svn")
     }
   end
-  if vim.fn.executable('lua-language-server') then
-    log.info("setting up lua-language-server...")
+
+  if vim.fn.executable('lua-language-server') > 0 then
+    log.info("setting up the lua-language-server lsp...")
     nvim_lsp.sumneko_lua.setup{
       cmd = {"lua-language-server"},
       root_dir = nvim_lsp.util.root_pattern(".git", ".svn")
     }
   end
 
-  if vim.fn.executable('clangd') then
-    log.info("setting up clangd...")
+  if vim.fn.executable('clangd') > 0 then
+    log.info("setting up the clangd lsp...")
     nvim_lsp.clangd.setup {
       cmd = {
         "clangd", "--all-scopes-completion=true", "--background-index=true",
