c69e2a53f6d9c2761407fdaadbdb6310a81a3841
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-09-01 14:23:02 -0400

    Added utils folder
    Added to it a logging module
    And a unit test module
    Added is_mod_available function

--- defaults/.config/nvim/lua/utils/utils.lua
+++ defaults/.config/nvim/lua/utils/utils.lua
@@ -1,7 +1,21 @@
 -- Similar to python's pprint. Usage: lua dump({1, 2, 3})
-function _G.dump(...)
+function dump(...)
     local objects = vim.tbl_map(vim.inspect, {...})
     print(unpack(objects))
+end
+
+function is_mod_available(name)
+    if package.loaded[name] then
+        return true
+    end
+    for _, searcher in ipairs(package.searchers or package.loaders) do
+        local loader = searcher(name)
+        if type(loader) == 'function' then
+            package.preload[name] = loader
+            return true
+        end
+    end
+    return false
 end
 
 local function create_augroups(definitions)
@@ -26,5 +40,7 @@ end
 -- create_augroups(autocmds)
 
 return {
-  create_augroups = create_augroups
+  create_augroups = create_augroups,
+  dump = dump,
+  is_mod_available = is_mod_available,
 }
