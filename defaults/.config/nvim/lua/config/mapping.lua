8bf806276935b650e69895ffb650c2b0d494dc1a
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-09-04 05:57:11 -0400

    Formatting

--- defaults/.config/nvim/lua/config/mapping.lua
+++ defaults/.config/nvim/lua/config/mapping.lua
@@ -8,7 +8,7 @@ M._modes = {'n', 'v', 'x', 's', 'o', '!', 'i', 'l', 'c', 't'}
 function M:_validate_mode(mode)
     log.trace("vali mode = ", mode)
     vim.validate {mode = {mode, 'string'}}
-    for _,m in pairs(self._modes) do if m == mode then return true end end
+    for _, m in pairs(self._modes) do if m == mode then return true end end
     return false
 end
 
@@ -16,7 +16,12 @@ function M:_noremap(lhs, rhs, opts, mode)
     log.trace("mode = ", mode)
     vim.validate {lhs = {lhs, 'string'}}
     vim.validate {opts = {opts, 'table'}}
-    vim.validate {mode = {mode, function(a) return self:_validate_mode(a) end, 'vim mapping mode'}}
+    vim.validate {
+        mode = {
+            mode, function(a) return self:_validate_mode(a) end,
+            'vim mapping mode'
+        }
+    }
     -- Always set mode to n
     -- Always add noremap to opts
     -- Do not modify orignal options
@@ -37,19 +42,19 @@ end
 
 -- Programatically setup all mode .. noremap functions. Such as {n,i,c}noremap
 function M:set()
-  for _, mode in ipairs(M._modes) do
-      -- See help: map!
-      if mode == '!' then
-          M['mapi'] = function(lhs, rhs, opts)
-              M:_noremap(lhs, rhs, opts, mode)
-          end
-      else
-          M[mode .. 'noremap'] = function(lhs, rhs, opts)
-              M:_noremap(lhs, rhs, opts, mode)
-          end
-      end
-  end
-  log.debug("mapping = ", M)
+    for _, mode in ipairs(M._modes) do
+        -- See help: map!
+        if mode == '!' then
+            M['mapi'] = function(lhs, rhs, opts)
+                M:_noremap(lhs, rhs, opts, mode)
+            end
+        else
+            M[mode .. 'noremap'] = function(lhs, rhs, opts)
+                M:_noremap(lhs, rhs, opts, mode)
+            end
+        end
+    end
+    log.debug("mapping = ", M)
 end
 
 return M
