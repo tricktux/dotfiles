b1aefd077a7e553d28be8594addc04f8fd2c0577
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-09-16 10:52:47 -0400

    Added work mappings

--- defaults/.config/nvim/lua/config/init.lua
+++ defaults/.config/nvim/lua/config/init.lua
@@ -7,16 +7,40 @@ local map = require('utils/keymap')
 local luv = vim.loop
 
 local home_dir = luv.os_homedir()
-
 local wikis = {
     home_dir .. '/Documents/wiki',
     home_dir .. '/External/reinaldo/resilio/wiki',
     '/mnt/samba/server/resilio/wiki'
 }
+local wikis_win = {
+    [[D:\Seafile\KnowledgeIsPower\wiki]],
+    [[D:\wiki]],
+    [[D:\Reinaldo\Documents\src\resilio\wiki]],
+}
+local data_folders = {'/sessions', '/ctags'}
+local cache_folders = {'/backup', '/swap', '/undofiles'}
+local wdev_path = [[D:/wings-dev]]
+local work_repos = {
+    ['1'] = [[/src/OneWings]],
+    ['2'] = [[/src/OneWings2]],
+    ['c'] = [[/config]],
+    ['s'] = [[/src]],
+}
 
-local data_folders = {'/sessions', '/ctags', '/backup', '/swap', '/undofiles'}
+local function _work_mappings(work_dir)
+    local map_pref = '<leader>ew'
+    local cmd_pref = '<cmd>lua require("utils.utils").file_fuzzer([['
+    local cmd_suff = ']])<cr>'
+    local opts = { silent = true }
+    for lhs, rhs in pairs(work_repos) do
+        log.trace("lhs = ", map_pref .. lhs, ", rhs = ",
+            cmd_pref .. work_dir .. rhs .. cmd_suff, ", opts = ", opts)
+        map.nnoremap(map_pref .. lhs,
+            cmd_pref .. work_dir .. rhs .. cmd_suff, opts)
+    end
+end
 
-local function find_dir(dirs)
+local function _find_dir(dirs)
     vim.validate {dirs = {dirs, 't'}}
     for _, dir in pairs(dirs) do
         log.trace("Potential dir: ", dir)
@@ -30,10 +54,20 @@ local function find_dir(dirs)
     return nil
 end
 
-local function config_win() end
+local function _config_win()
+    local wiki = _find_dir(wikis)
+    log.info("wiki = ", wiki)
+    if wiki ~= nil then vim.g.wiki_path = wiki end
 
-local function config_unix()
-    local wiki = find_dir(wikis)
+    vim.g.browser_cmd = 'firefox.exe'
+
+    if utl.isdir(wdev_path) then
+        _work_mappings(wdev_path)
+    end
+end
+
+local function _config_unix()
+    local wiki = _find_dir(wikis)
     log.info("wiki = ", wiki)
     if wiki ~= nil then vim.g.wiki_path = wiki end
 
@@ -42,13 +76,15 @@ end
 
 local function _init()
     log.info('--- Start Neovim Run ---')
+
+    map:set()  -- Set mappings
+
     if utl.has_unix() then
-        config_unix()
+        _config_unix()
     else
-        config_win()
+        _config_win()
     end
 
-    -- For now still call init.vim
     if vim.fn['plugin#Config']() ~= 1 then
         log.error('No plugins were loaded')
     end
@@ -56,6 +92,9 @@ local function _init()
     -- Create needed directories if they don't exist already
     for _, folder in pairs(data_folders) do
         luv.fs_mkdir(vim.g.std_data_path .. folder, 777)
+    end
+    for _, folder in pairs(cache_folders) do
+        luv.fs_mkdir(vim.g.std_cache_path .. folder, 777)
     end
 
     vim.fn['mappings#Set']()
@@ -67,8 +106,7 @@ end
 -- This function is called during the VimEnter event. From the 
 -- plugin#AfterConfig function. This is done to ensure that variables and lua 
 -- modules have been loaded for sure
-local function after_init()
-    map:set()
+local function _after_init()
     cpl.compl:set()
     cpl.diagn:set()
     lsp.set()
@@ -76,4 +114,4 @@ local function after_init()
     -- plg.setup()
 end
 
-return {init = _init, after_vim_enter = after_init}
+return {init = _init, after_vim_enter = _after_init}
