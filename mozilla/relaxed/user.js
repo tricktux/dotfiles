7d2712a62bbe81efd1e768e5c3a78dbf25d8d586
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-11-01 16:31:10 -0500

    Updating gpu and perf opts

--- mozilla/relaxed/user.js
+++ mozilla/relaxed/user.js
@@ -33,10 +33,21 @@ user_pref("mousewheel.system_scroll_override_on_root_content.vertical.factor", 1
 user_pref("toolkit.scrollbox.horizontalScrollDistance", 6);
 user_pref("toolkit.scrollbox.verticalScrollDistance", 2);
 
+// Disable pocket
+user_pref("extensions.pocket.enabled", false)
+
 // Performance
+// GPU hardware acceleration
+user_pref("gfx.canvas.azure.accelerated", true)
+
+// Cache to ram
+// user_perf('browser.cache.disk.enable', true);
+// 1000 is user UID, get it with id -u
+// user_perf('browser.cache.disk.parent_directory', "/run/user/1000/firefox");
+// Either or
 user_perf('browser.cache.disk.enable', false);
 user_perf('browser.cache.memory.enable', true);
-user_perf('browser.cache.memory.max_entry_size',2000000);
+user_perf('browser.cache.memory.max_entry_size',-1);
 user_perf('browser.sessionstore.interval', 600000);
 
 // New tab:
