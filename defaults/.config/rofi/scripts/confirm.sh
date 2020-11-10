e183b877e7216fb3e999ab93ed47ebeb08aa6007
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-11-10 06:26:25 -0500

    Confirm use proper rofi config

--- defaults/.config/rofi/scripts/confirm.sh
+++ defaults/.config/rofi/scripts/confirm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 message="${1:-"Confirm?"}"
-response=$(echo -e "No\nYes" | rofi -dmenu -i -p "$message ")
+response=$(echo -e "No\nYes" | rofi -config $XDG_CONFIG_HOME/rofi/$(hostname) -dmenu -i -p "$message ")
 
 if [ "$response" = "Yes" ]; then
-  exit 0;
+  exit 0
 else
-  exit 1;
+  exit 1
 fi
