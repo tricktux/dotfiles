5e42e873b246f79da3fd5d77ac2377ad6fd8692f
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-11-10 06:37:56 -0500

    Use sidebar theme for tasks

--- defaults/.config/rofi/scripts/myrmidon.sh
+++ defaults/.config/rofi/scripts/myrmidon.sh
@@ -5,8 +5,14 @@ cwd=$(echo $(dirname $0))
 config_file="${1:-"$HOME/.myrmidon-tasks.json"}"
 tasks=$(cat $config_file)
 
+prompt="Tasks:"
+
+rofi_cmd="rofi -config $XDG_CONFIG_HOME/rofi/$(hostname) \
+  -theme /usr/share/rofi/themes/sidebar.rasi
+  -dmenu -sort -matching fuzzy -i -p $prompt"
+
 # Pass tasks to rofi, and get the output as the selected option
-selected=$(echo $tasks | jq -j 'map(.name) | join("\n")' | rofi -config $XDG_CONFIG_HOME/rofi/$(hostname) -dmenu -sort -matching fuzzy -i -p "Search tasks")
+selected=$(echo $tasks | jq -j 'map(.name) | join("\n")' | $rofi_cmd)
 task=$(echo $tasks | jq ".[] | select(.name == \"$selected\")")
 
 # Exit if no task was found
