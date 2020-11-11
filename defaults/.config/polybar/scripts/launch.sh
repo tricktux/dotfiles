c55ed7d46ed47e9a3f2063cf7ae38e86a19694f3
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-11-11 07:50:40 -0500

    Give it some time to die

--- defaults/.config/polybar/scripts/launch.sh
+++ defaults/.config/polybar/scripts/launch.sh
@@ -2,12 +2,18 @@
 
 machine=$(hostname)
 
+killall -q polybar
+killall -q polybar
+killall -q polybar
+
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do
   # Terminate already running bar instances
   killall -q polybar
-  sleep 0.001
+  sleep 0.5
 done
+
+sleep 1
 
 # Sun Mar 17 2019 15:06
 # Do not enable --reload. Consumes power
