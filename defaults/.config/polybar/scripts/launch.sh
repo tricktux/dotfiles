84e82b2f5ed3c54dc7c2ce379e2dafbdae5c9e69
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-11-11 06:24:34 -0500

    Efficiently kill polybar

--- defaults/.config/polybar/scripts/launch.sh
+++ defaults/.config/polybar/scripts/launch.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
 
-machine=`hostname`
-# Terminate already running bar instances
-killall -q polybar
+machine=$(hostname)
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do
+  # Terminate already running bar instances
+  killall -q polybar
+  sleep 0.001
+done
 
 # Sun Mar 17 2019 15:06
 # Do not enable --reload. Consumes power
-polybar top-$machine&
-polybar bottom-$machine&
+polybar top-$machine &
+polybar bottom-$machine &
