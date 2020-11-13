123015fcd803ad4a468a5e057d10dca6dc480b31
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2020-11-13 07:07:05 -0500

    Update switches and options

--- defaults/.config/polybar/scripts/flux_config.lua
+++ defaults/.config/polybar/scripts/flux_config.lua
@@ -2,18 +2,18 @@ local _M = {}
 
 -- Redshift command. Basically how to run the command.
 -- Switches are provided as its own table
-_M.redshift = {cmd = [[/usr/bin/redshift]], switches = {"-l", "manual"}}
+_M.redshift = {cmd = [[/usr/bin/redshift]], switches = {"-p", "-l", "manual"}}
 
 -- Wal command. But with switches for day time and switches for night time
 -- Switches are provided as its own table
 _M.pywal = {
   cmd = [[/usr/bin/wal]],
   day = {
-    "--theme", "base16-google", "-l", "-o",
+    "--theme", "base16-google", "-l", "-q", "-o",
     "/home/reinaldo/.config/polybar/launch.sh"
   },
   night = {
-    "--theme", "base16-gruvbox-hard", "-o",
+    "--theme", "base16-gruvbox-hard", "-q", "-o",
     "/home/reinaldo/.config/polybar/launch.sh"
   }
 }
