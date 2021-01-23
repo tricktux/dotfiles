59715e273c5f968b29d74f07d14bfbe35a1355dd
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2021-01-22 23:55:58 -0500

    Switching to isync
    Before starting neomutt
    - Synchronize email initially with isync
    - Update notmuch
    - Kickstart the imapnotify programs
    
    Only then proceed to start the neomutt sessions

--- defaults/.config/tmux/neomutt.sh
+++ defaults/.config/tmux/neomutt.sh
@@ -2,6 +2,19 @@
 
 session="neomutt"
 
+# Fail as soon as a command fails
+set -Eeuo pipefail
+
+# Execute cleanup() if any of these sig events happen
+trap cleanup SIGINT SIGTERM ERR #EXIT
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR #EXIT
+  notify-send 'Email' "Something went wrong..." -u critical
+  read -n1 -r key
+  exit $?
+}
+
 # -t is the target, see man tmux, COMMANDS section
 #   You can also refer to a windows in a session. I.e: -t $session:0
 #   That is windows 0 in $session
@@ -10,16 +23,26 @@ session="neomutt"
 # Start the tmux server, if not already running, without creating any sessions.
 /usr/bin/tmux start-server
 
+# Synchronizing email
+notify-send 'Email' 'Synchronizing...'
+/usr/bin/mbsync -D -ac ~/.config/isync/mbsyncrc >! /tmp/isync.log 2>&1
+/usr/bin/goimapnotify -conf ~/.config/imapnotify/gmail.conf \
+  >! /tmp/imapnotify_gmail.log 2>&1 &
+/usr/bin/goimapnotify -conf ~/.config/imapnotify/molinamail.conf \
+  >! /tmp/imapnotify_molinamail.log 2>&1 &
+
+notify-send 'Email' 'Starting session...'
+
 /usr/bin/tmux new-session -d -s $session -n 'mailserver' \
   'neomutt -F ~/.config/neomutt/user.molinamail'
 /usr/bin/tmux new-window -d -t $session -n 'gmail' \
   'neomutt -F ~/.config/neomutt/user.gmail'
 /usr/bin/tmux new-window -d -t $session -n 'ufl' \
   'neomutt -F ~/.config/neomutt/user.ufl'
-# Wed Feb 19 2020 20:11: Account has been disabled 
+# Wed Feb 19 2020 20:11: Account has been disabled
 # /usr/bin/tmux  new-window  -d -t $session -n 'psu' 'neomutt -F ~/.config/neomutt/user.psu'
-# Tue May 19 2020 04:51: Free account doesn't allow this stuff 
+# Tue May 19 2020 04:51: Free account doesn't allow this stuff
 # /usr/bin/tmux new-window -d -t $session -n 'pmserver' 'protonmail-bridge --cli'
 # /usr/bin/tmux new-window -d -t $session -n 'pm' \
-  # 'sleep 5; neomutt -F ~/.config/neomutt/user.pm'
+# 'sleep 5; neomutt -F ~/.config/neomutt/user.pm'
 /usr/bin/tmux -2 attach-session -t $session
