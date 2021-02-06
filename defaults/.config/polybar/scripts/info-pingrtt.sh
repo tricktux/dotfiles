45480e9c747b0f5b8c39a623962e4e2557521ffa
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2021-02-06 12:58:31 -0500

    Improved ping script
    Now it reports upload and down speed also

--- defaults/.config/polybar/scripts/info-pingrtt.sh
+++ defaults/.config/polybar/scripts/info-pingrtt.sh
@@ -2,15 +2,14 @@
 
 HOST=1.1.1.1
 
-# simpletest --simple
-# rtt=$(echo "$data" | sed -rn 's/.*Ping: ([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
-# down=$(echo "$data" | sed -rn 's/.*Download: ([0-9]{1,})\.?[0-9]{0,} Mbit\/s.*/\1/p')
-# up=$(echo "$data" | sed -rn 's/.*Upload: ([0-9]{1,})\.?[0-9]{0,} Mbit\/s.*/\1/p')
-if ! ping=$(ping -n -c 1 -W 1 $HOST); then
+if ! ping=$(speedtest --simple); then
     rtt="--"
     icon="%{F#d60606}%{F-}"
 else
-    rtt=$(echo "$ping" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
+    rtt=$(echo "$ping" | sed -rn 's/.*Ping: ([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
+    down=$(echo "$ping" | sed -rn 's/.*Download: ([0-9]{1,})\.?[0-9]{0,} Mbit\/s.*/\1/p')
+    up=$(echo "$ping" | sed -rn 's/.*Upload: ([0-9]{1,})\.?[0-9]{0,} Mbit\/s.*/\1/p')
+    out="$rtt ms $down Mbit/s $up Mbit/s"
 
     if [ "$rtt" -lt 50 ]; then
         icon="%{F#3cb703}%{F-}"
@@ -20,4 +19,4 @@ else
         icon="%{F#d60606}%{F-}"
     fi
 fi
-echo "$icon $rtt ms"
+echo "$icon $out"
