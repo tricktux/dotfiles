daffbd05bd9945e5153b18763c7672217aec33ed
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2021-03-19 11:19:43 -0400

    Added support for nvim-lightbulb

--- defaults/.config/nvim/lua/config/plugin.lua
+++ defaults/.config/nvim/lua/config/plugin.lua
@@ -199,6 +199,48 @@ local function setup_scrollbar()
   aug.create(au)
 end
 
+local function setup_lightbulb()
+  if not utl.is_mod_available('nvim-lightbulb') then
+    api.nvim_err_writeln('nvim-lightbulb module not available')
+    return
+  end
+
+  vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
+
+  require'nvim-lightbulb'.update_lightbulb {
+    sign = {
+        enabled = true,
+        -- Priority of the gutter sign
+        priority = 10,
+    },
+    float = {
+        enabled = false,
+        -- Text to show in the popup float
+        text = "💡",
+        -- Available keys for window options:
+        -- - height     of floating window
+        -- - width      of floating window
+        -- - wrap_at    character to wrap at for computing height
+        -- - max_width  maximal width of floating window
+        -- - max_height maximal height of floating window
+        -- - pad_left   number of columns to pad contents at left
+        -- - pad_right  number of columns to pad contents at right
+        -- - pad_top    number of lines to pad contents at top
+        -- - pad_bottom number of lines to pad contents at bottom
+        -- - offset_x   x-axis offset of the floating window
+        -- - offset_y   y-axis offset of the floating window
+        -- - anchor     corner of float to place at the cursor (NW, NE, SW, SE)
+        -- - winblend   transparency of the window (0-100)
+        win_opts = {},
+    },
+    virtual_text = {
+        enabled = false,
+        -- Text to show at virtual text
+        text = "💡",
+    }
+  }
+end
+
 local function setup_telescope()
   if not utl.is_mod_available('telescope') then
     api.nvim_err_writeln('telescope module not available')
@@ -468,6 +510,11 @@ function _packer:setup()
   use {
     'kyazdani42/nvim-tree.lua',
     config = require('config.plugins.tree_explorer').nvimtree_config()
+  }
+
+  use {
+    'kosayoda/nvim-lightbulb',
+    config = setup_lightbulb()
   }
 end
 
