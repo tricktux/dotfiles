7d6bbfd56246c60f689790053bf285cf9a1ee1b8
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2021-06-12 11:51:57 -0400

    Bring cmus back

--- defaults/.config/tmux/scratch.sh
+++ defaults/.config/tmux/scratch.sh
@@ -14,6 +14,6 @@ session="scratch"
 # /usr/bin/tmux new-window -d -t $session -n \
   # 'journal' '~/.config/i3/scripts/journal.sh'
 # Vim satisfies most of needs
-/usr/bin/tmux new-window -d -t $session -n 'calc' '/usr/bin/bc -q'
-# /usr/bin/tmux new-window -d -t $session -n 'cmus' '/usr/bin/cmus'
+# /usr/bin/tmux new-window -d -t $session -n 'calc' '/usr/bin/bc -q'
+/usr/bin/tmux new-window -d -t $session -n 'cmus' '/usr/bin/cmus'
 /usr/bin/tmux attach-session -t $session
