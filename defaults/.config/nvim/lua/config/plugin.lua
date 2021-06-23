a981f1fb32b0359a1adde3a6d4cfba9e695d2140
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2021-06-23 10:08:22 -0400

    chore(packer): remove huge default config

--- defaults/.config/nvim/lua/config/plugin.lua
+++ defaults/.config/nvim/lua/config/plugin.lua
@@ -698,59 +698,9 @@ local _packer = {}
 _packer._path = vim.g.std_data_path .. [[/site/pack/packer/start/packer.nvim]]
 _packer._repo = [[https://github.com/wbthomason/packer.nvim]]
 _packer._config = {
-  ensure_dependencies = true, -- Should packer install plugin dependencies?
-  package_root = require('packer.util').join_paths(vim.fn.stdpath('data'),
-                                                   'site', 'pack'),
   compile_path = require('packer.util').join_paths(vim.fn.stdpath('data'),
                                                    'site', 'plugin',
                                                    'packer_compiled.vim'),
-  plugin_package = 'packer', -- The default package for plugins
-  max_jobs = nil, -- Limit the number of simultaneous jobs. nil means no limit
-  auto_clean = true, -- During sync(), remove unused plugins
-  compile_on_sync = true, -- During sync(), run packer.compile()
-  disable_commands = false, -- Disable creating commands
-  opt_default = false, -- Default to using opt (as opposed to start) plugins
-  transitive_opt = true, -- Make dependencies of opt plugins also opt by default
-  transitive_disable = true, -- Automatically disable dependencies of disabled plugins
-  auto_reload_compiled = true, -- Automatically reload the compiled file after creating it.
-  git = {
-    cmd = 'git', -- The base command for git operations
-    subcommands = { -- Format strings for git subcommands
-      update = '-C %s pull --ff-only --progress --rebase=false',
-      install = 'clone %s %s --depth %i --no-single-branch --progress',
-      fetch = '-C %s fetch --depth 999999 --progress',
-      checkout = '-C %s checkout %s --',
-      update_branch = '-C %s merge --ff-only @{u}',
-      current_branch = '-C %s branch --show-current',
-      diff = '-C %s log --color=never --pretty=format:FMT --no-show-signature HEAD@{1}...HEAD',
-      diff_fmt = '%%h %%s (%%cr)',
-      get_rev = '-C %s rev-parse --short HEAD',
-      get_msg = '-C %s log --color=never --pretty=format:FMT --no-show-signature HEAD -n 1',
-      submodules = '-C %s submodule update --init --recursive --progress'
-    },
-    depth = 1, -- Git clone depth
-    clone_timeout = 60 -- Timeout, in seconds, for git clones
-  },
-  display = {
-    non_interactive = false, -- If true, disable display windows for all operations
-    open_fn = nil, -- An optional function to open a window for packer's display
-    open_cmd = '65vnew [packer]', -- An optional command to open a window for packer's display
-    working_sym = '⟳', -- The symbol for a plugin being installed/updated
-    error_sym = '✗', -- The symbol for a plugin with an error in installation/updating
-    done_sym = '✓', -- The symbol for a plugin which has completed installation/updating
-    removed_sym = '-', -- The symbol for an unused plugin which was removed
-    moved_sym = '→', -- The symbol for a plugin which was moved (e.g. from opt to start)
-    header_sym = '━', -- The symbol for the header line in packer's display
-    show_all_info = true, -- Should packer show all update details automatically?
-    keybindings = { -- Keybindings for the display window
-      quit = 'q',
-      toggle_info = '<CR>',
-      prompt_revert = 'r'
-    }
-  },
-  luarocks = {
-    python_cmd = 'python' -- Set the python command to use for running hererocks
-  }
 }
 
 function _packer:download()
