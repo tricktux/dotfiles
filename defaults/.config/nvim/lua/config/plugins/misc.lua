d7f7fc7faca2735a71fa7769761d1f852da7b9d2
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2021-09-03 15:55:35 -0400

    fix(zen-mode): better screen ratio

--- defaults/.config/nvim/lua/config/plugins/misc.lua
+++ defaults/.config/nvim/lua/config/plugins/misc.lua
@@ -13,7 +13,7 @@ function M.setup_zen_mode()
       -- * an absolute number of cells when > 1
       -- * a percentage of the width / height of the editor when <= 1
       -- * a function that returns the width or the height
-      width = .70, -- width of the Zen window
+      width = .85, -- width of the Zen window
       height = 1, -- height of the Zen window
       -- by default, no options are changed for the Zen window
       -- uncomment any of the options below, or add other vim.wo options you want to apply
