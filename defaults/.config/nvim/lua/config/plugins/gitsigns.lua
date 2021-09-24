ba9e9dc45dccbb38cc631289e1700719b4616273
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2021-09-24 14:13:12 -0400

    fix: deprecated option

--- defaults/.config/nvim/lua/config/plugins/gitsigns.lua
+++ defaults/.config/nvim/lua/config/plugins/gitsigns.lua
@@ -100,7 +100,7 @@ function M.setup()
     -- Kinda annoying
     numhl = false,
     keymaps = nil,
-    watch_index = {interval = 100, follow_files = true},
+    watch_gitdir = {interval = 100, follow_files = true},
     sign_priority = 6,
     status_formatter = format_status,
     on_attach = on_attach
