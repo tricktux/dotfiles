c33e2884146548be635f13aa2579374f5aa64b05
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2021-10-05 09:04:29 -0400

    fix: perform the same commands as regular config, just dont restart anything

--- defaults/.config/polybar/scripts/flux_/flux_init_config.lua
+++ defaults/.config/polybar/scripts/flux_/flux_init_config.lua
@@ -7,11 +7,24 @@ _M.redshift = {cmd = [[/usr/bin/redshift]], switches = {"-p", "-l", "manual"}}
 -- Wal command. But with switches for day time and switches for night time
 -- Switches are provided as its own table
 _M.pywal = {
-  cmd = [[/usr/bin/wal]],
-  day = {"--theme", "base16-google", "-l", "-q"},
-  night = {"--theme", "base16-gruvbox-hard", "-q"},
-  sunset = {"--theme", "solarized", "-q"},
-  sunrise = {"--theme", "solarized", "-l", "-q"}
+  -- Skip reloading stuff
+  cmd = [[/usr/bin/wal -e]],
+  day = {
+    "--theme", "base16-google", "-l", "-o",
+    [["$HOME/.config/polybar/scripts/flux_/flux_post_day"]]
+  },
+  sunrise = {
+    "--theme", "solarized", "-l", "-o",
+    [["$HOME/.config/polybar/scripts/flux_/flux_post_sunrise"]]
+  },
+  night = {
+    "--theme", "base16-gruvbox-hard", "-o",
+    [["$HOME/.config/polybar/scripts/flux_/flux_post_night"]]
+  },
+  sunset = {
+    "--theme", "solarized", "-o",
+    [["$HOME/.config/polybar/scripts/flux_/flux_post_sunset"]]
+  }
 }
 
 _M.print = {enabled = false}
