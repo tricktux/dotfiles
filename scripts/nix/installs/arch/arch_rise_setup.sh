a763a122778e5bc1e0c6445f0423087b8f2faf93
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2021-10-17 11:30:58 -0400

    fix(ranger): use ranger-git

--- scripts/nix/installs/arch/arch_rise_setup.sh
+++ scripts/nix/installs/arch/arch_rise_setup.sh
@@ -305,7 +305,7 @@ paci --needed --noconfirm {ttf,otf}-fira-{code,mono} {ttf,otf}-font-awesome \
 paci --needed --noconfirm ttf-inconsolata
 paci --needed --noconfirm xorg-xfontsel gtk2fontsel
 # Package doesn't exist anymore thumbnailer 
-paci --needed --noconfirm atool ranger zip unzip w3m ffmpeg highlight libcaca python-pillow
+paci --needed --noconfirm atool ranger-git zip unzip w3m ffmpeg highlight libcaca python-pillow
 # Not installing anymore: advcp 
 paci --needed --noconfirm mediainfo odt2txt poppler w3m bat exa fzf fd \
   ripgrep tmux imagemagick ghostscript xclip
