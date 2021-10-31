fc4b810de86b826806a6e24cc2c1ac85f18542d4
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2021-10-31 18:52:54 -0400

    fix: depracated options

--- defaults/.config/nvim/lua/config/plugins/tree_explorer.lua
+++ defaults/.config/nvim/lua/config/plugins/tree_explorer.lua
@@ -64,11 +64,11 @@ function M.nvimtree_config()
   }
   vim.g.nvim_tree_side = 'left' -- left by default
   vim.g.nvim_tree_width = 40 -- 30 by default
-  vim.g.nvim_tree_ignore = {'.git', 'node_modules', '.cache', '.svn'} -- empty by default
+  -- vim.g.nvim_tree_ignore = {'.git', 'node_modules', '.cache', '.svn'} -- empty by default
   vim.g.nvim_tree_auto_ignore_ft = {'startify', 'dashboard'} -- empty by default, don't auto open tree on specific filetypes.
   vim.g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
   vim.g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
-  vim.g.nvim_tree_hide_dotfiles = 0 -- 0 by default, this option hides files and folders starting with a dot `.`
+  -- vim.g.nvim_tree_hide_dotfiles = 0 -- 0 by default, this option hides files and folders starting with a dot `.`
   vim.g.nvim_tree_git_hl = 0 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
   vim.g.nvim_tree_root_folder_modifier = ':~' -- This is the default. See :help filename-modifiers for more options
   vim.g.nvim_tree_width_allow_resize = 1 -- 0 by default, will not resize the tree when opening a file
