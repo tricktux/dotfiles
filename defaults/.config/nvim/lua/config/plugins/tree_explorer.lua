a38f89a8e114d4e97eb480fdc6037ca3c9559715
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2021-11-12 11:11:39 -0500

    fix(nvim-tree): fixed and updated options

--- defaults/.config/nvim/lua/config/plugins/tree_explorer.lua
+++ defaults/.config/nvim/lua/config/plugins/tree_explorer.lua
@@ -9,12 +9,30 @@ function M.nvimtree_config()
     return
   end
 
+  -- These additional options must be set **BEFORE** calling `require'nvim-tree'` or calling setup.
+  vim.g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
+  vim.g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
+  vim.g.nvim_tree_git_hl = 0 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
+  vim.g.nvim_tree_highlight_opened_files = 1  -- 0 by default, will enable folder and file icon highlight for opened files/directories.
+  vim.g.nvim_tree_root_folder_modifier = ':~' -- This is the default. See :help filename-modifiers for more options
+  vim.g.nvim_tree_width_allow_resize = 1 -- 0 by default, will not resize the tree when opening a file
+  vim.g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
+  vim.g.nvim_tree_show_icons = {['git'] = 0, ['folders'] = 0, ['files'] = 0}
+  vim.g.nvim_tree_group_empty = 1 -- 0 by default, compact folders that only contain a single folder into one node in the file tree
+  vim.g.nvim_tree_disable_window_picker = 1 -- 0 by default, will disable the window picker.
+  -- one space by default, used for rendering the space between the icon and 
+  -- the filename. Use with caution, it could break rendering if you set an 
+  -- empty string depending on your font.
+  vim.g.nvim_tree_icon_padding = ' ' 
+  vim.g.nvim_tree_special_files = { ['README.md'] = 1, ['Makefile'] = 1, ['MAKEFILE'] = 1 } -- List of filenames that gets highlighted with NvimTreeSpecialFile
+
   local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+
   require'nvim-tree'.setup {
     disable_netrw = true,
     hijack_netrw = true,
     open_on_setup = false,
-    ignore_ft_on_setup = {},
+    ignore_ft_on_setup = {'startify', 'dashboard'},
     auto_close = true,
     auto_open = true,
     tab_open = true,
@@ -22,8 +40,25 @@ function M.nvimtree_config()
     open_on_tab = false,
     hijack_cursor = false,
     update_cwd = false,
-    update_focused_file = {enable = false, update_cwd = false, ignore_list = {}},
+    update_focused_file = {enable = true, update_cwd = true, ignore_list = {}},
     system_open = {cmd = nil, args = {}},
+    filters = {
+      dotfiles = false,
+      custom = {}
+    },
+    diagnostics = {
+      enable = false,
+      icons = {
+        hint = "",
+        info = "",
+        warning = "",
+        error = "",
+      }
+    },
+    update_to_buf_dir   = {
+      enable = true,
+      auto_open = true,
+    },
     view = {
       width = 30,
       side = 'left',
@@ -62,18 +97,6 @@ function M.nvimtree_config()
       }
     }
   }
-  vim.g.nvim_tree_side = 'left' -- left by default
-  vim.g.nvim_tree_width = 40 -- 30 by default
-  -- vim.g.nvim_tree_ignore = {'.git', 'node_modules', '.cache', '.svn'} -- empty by default
-  vim.g.nvim_tree_auto_ignore_ft = {'startify', 'dashboard'} -- empty by default, don't auto open tree on specific filetypes.
-  vim.g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
-  vim.g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
-  -- vim.g.nvim_tree_hide_dotfiles = 0 -- 0 by default, this option hides files and folders starting with a dot `.`
-  vim.g.nvim_tree_git_hl = 0 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
-  vim.g.nvim_tree_root_folder_modifier = ':~' -- This is the default. See :help filename-modifiers for more options
-  vim.g.nvim_tree_width_allow_resize = 1 -- 0 by default, will not resize the tree when opening a file
-  vim.g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
-  vim.g.nvim_tree_show_icons = {['git'] = 0, ['folders'] = 0, ['files'] = 0}
 
   require('which-key').register {
     ["<plug>file_browser"] = {require('nvim-tree').toggle, "file_browser"}
