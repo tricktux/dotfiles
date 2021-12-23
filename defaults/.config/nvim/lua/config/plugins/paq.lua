97143128360de57e5f65522fd3ae85fd9977fb40
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2021-12-23 13:27:55 -0500

    feat(plugins): initial support for paq plugin
    - There are a few things wrong with `packer`
      - Stuff not found
      - Strange to say the least
    - But it's hard to beat it's simplicity and nice DSL for configuring
      plugins
    - It's hard to contain everything under one single table in paq
    - So abandoning it for now

---
+++ defaults/.config/nvim/lua/config/plugins/paq.lua
@@ -0,0 +1,56 @@
+local utl = require('utils.utils')
+local api = vim.api
+
+local M = {}
+M.__path = vim.g.std_data_path .. [[/site/pack/packer/start/paq-nvim]]
+M.__repo = [[https://github.com/savq/paq-nvim]]
+
+M.__plugins = {
+  [1] = {
+    paq = {"savq/paq-nvim"},
+  },
+  [2] = {
+    paq = {"folke/which-key.nvim"},
+    config = require('config.plugins.whichkey').setup
+  },
+  [3] = {
+    paq = {{'nvim-lua/popup.nvim'}, {'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+  },
+  [4] = {
+    paq = {'nvim-lua/plenary.nvim'}
+  },
+}
+
+
+function M:__bootstrap()
+  if vim.fn.empty(vim.fn.glob(self.__path)) <= 0 then
+    return false
+  end
+
+  vim.fn.system {
+    'git',
+    'clone',
+    '--depth=1',
+    self.__repo,
+    self.__path
+  }
+
+  vim.cmd('packadd paq-nvim')
+  local paq = require('paq')
+  -- Exit nvim after installing plugins
+  vim.cmd('autocmd User PaqDoneInstall quit')
+  -- Read and install packages
+  paq(PKGS)
+  paq.install()
+  return true
+end
+
+function M:setup()
+  if self:__bootstrap() == true then
+    return
+  end
+  -- Load Paq
+  local paq = require('paq')(PKGS)
+end
+
+return M
