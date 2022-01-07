7a265f02e0172dc8b4fd7934bdd0527e090389de
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2022-01-07 09:54:20 -0500

    fix(diag): prevent from printing long lines

--- defaults/.config/nvim/lua/config/lsp.lua
+++ defaults/.config/nvim/lua/config/lsp.lua
@@ -144,13 +144,7 @@ local function print_diagnostics(opts, bufnr, line_nr)
   local line_diagnostics = vim.diagnostic.get(bufnr, opts)
   if vim.tbl_isempty(line_diagnostics) then return end
 
-  local diagnostic_message = ""
-  for i, diagnostic in ipairs(line_diagnostics) do
-    diagnostic_message = diagnostic_message .. string.format("%d: %s", i, diagnostic.message or "")
-    if i ~= #line_diagnostics then
-      diagnostic_message = diagnostic_message .. " -- "
-    end
-  end
+  local diagnostic_message = string.format("[%d/%d]: %s", 1, #line_diagnostics, line_diagnostics[1] or "")
   vim.api.nvim_echo({{diagnostic_message, "Normal"}}, false, {})
 end
 
