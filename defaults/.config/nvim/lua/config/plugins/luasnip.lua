1d2fd80192c7db3bf309005b87b3fbc29ab14cbb
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2022-03-15 21:06:54 -0400

    chore: formatting

--- defaults/.config/nvim/lua/config/plugins/luasnip.lua
+++ defaults/.config/nvim/lua/config/plugins/luasnip.lua
@@ -1,4 +1,3 @@
-
 local M = {}
 
 function M:config()
@@ -19,54 +18,41 @@ function M:config()
     -- Crazy highlights!!
     -- ext_opts = nil,
     ext_opts = {
-      [types.choiceNode] = {
-        active = {
-          virt_text = {{"●", "GruvboxOrange"}}
-        }
-      },
-      [types.insertNode] = {
-        active = {
-          virt_text = {{"●", "GruvboxBlue"}}
-        }
-      }
-    },
+      [types.choiceNode] = {active = {virt_text = {{"●", "GruvboxOrange"}}}},
+      [types.insertNode] = {active = {virt_text = {{"●", "GruvboxBlue"}}}}
+    }
   }
 
-  ls.filetype_extend("all", { "_" })
+  ls.filetype_extend("all", {"_"})
   require("luasnip.loaders.from_vscode").lazy_load()
   require("luasnip.loaders.from_snipmate").lazy_load()
   -- Load snippets from my-snippets folder
-  -- If path is not specified, luasnip will look for the `snippets` directory 
+  -- If path is not specified, luasnip will look for the `snippets` directory
   -- in rtp (for custom-snippet probably
   -- -- `~/.config/nvim/snippets`).
-  require("luasnip.loaders.from_snipmate").load({ path = { vim.g.std_config_path .. [[/snippets/]] } })
+  require("luasnip.loaders.from_snipmate").load({
+    path = {vim.g.std_config_path .. [[/snippets/]]}
+  })
 
   local wk = require("which-key")
   local mappings = {
     -- <c-k> is my expansion key
     -- this will expand the current item or jump to the next item within the snippet.
     ["<c-k>"] = {
-      function()
-        if ls.expand_or_jumpable() then
-          ls.expand_or_jump()
-        end
-      end, "expand_snippet"},
-      -- <c-j> is my jump backwards key.
-      -- this always moves to the previous item within the snippet
+      function() if ls.expand_or_jumpable() then ls.expand_or_jump() end end,
+      "expand_snippet"
+    },
+    -- <c-j> is my jump backwards key.
+    -- this always moves to the previous item within the snippet
     ["<c-j>"] = {
-      function()
-        if ls.jumpable(-1) then
-          ls.jump(-1)
-        end
-      end, "in_snippet_prev"},
-        -- <c-l> is selecting within a list of options.
-        -- This is useful for choice nodes (introduced in the forthcoming episode 2)
+      function() if ls.jumpable(-1) then ls.jump(-1) end end, "in_snippet_prev"
+    },
+    -- <c-l> is selecting within a list of options.
+    -- This is useful for choice nodes (introduced in the forthcoming episode 2)
     ["<c-l>"] = {
-      function()
-        if ls.choice_active() then
-          ls.change_choice(1)
-        end
-      end, "in_snippet_select"},
+      function() if ls.choice_active() then ls.change_choice(1) end end,
+      "in_snippet_select"
+    }
   }
   wk.register(mappings, {mode = "i"})
   wk.register(mappings, {mode = "s"})
