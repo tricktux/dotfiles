a1866d7c9913871dff37912e29ac1f48c8a80e72
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2022-03-27 13:15:44 -0400

    fix(augroup): move to the new lua api

--- defaults/.config/nvim/lua/config/augroups.lua
+++ defaults/.config/nvim/lua/config/augroups.lua
@@ -3,19 +3,19 @@ local api = vim.api
 
 local function create(definitions)
   for group_name, definition in pairs(definitions) do
-    log.trace("Setting augroup = ", group_name)
+    log.info("Setting augroup = ", group_name)
     vim.cmd('augroup ' .. group_name)
     vim.cmd('autocmd!')
     for _, def in ipairs(definition) do
       local command = table.concat(vim.tbl_flatten {'autocmd', def}, ' ')
-      log.trace(command)
+      log.info(command)
       vim.cmd(command)
     end
     vim.cmd('augroup END')
   end
 end
 
-local function setup()
+local function deprecated()
   create {
     highlight_yank = {
       {
@@ -24,6 +24,20 @@ local function setup()
       }
     }
   }
+end
+
+local function setup()
+  if vim.fn.has('nvim-0.7') <= 0 then
+    deprecated()
+    return
+  end
+
+  local id = api.nvim_create_augroup('highlight_yank', {clear = true})
+  api.nvim_create_autocmd('TextYankPost', {
+    callback = function() vim.highlight.on_yank {timeout = 250} end,
+    pattern = '*',
+    group = id
+  })
 end
 
 return {create = create, setup = setup}
