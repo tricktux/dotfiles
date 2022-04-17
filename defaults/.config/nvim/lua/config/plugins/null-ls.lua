819a01d9c3416bd614324fcf618203508abb8664
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2022-04-17 16:46:05 -0400

    chore: formatting

--- defaults/.config/nvim/lua/config/plugins/null-ls.lua
+++ defaults/.config/nvim/lua/config/plugins/null-ls.lua
@@ -4,22 +4,15 @@ local M = {}
 --  
 M.plantuml = {
   name = "plantuml",
-  filetypes = { ["plantuml"] = true },
-  methods = { [require("null-ls").methods.DIAGNOSTICS] = true },
-  generator = {
-    fn = function()
-      return "I am a source!"
-    end,
-  },
+  filetypes = {["plantuml"] = true},
+  methods = {[require("null-ls").methods.DIAGNOSTICS] = true},
+  generator = {fn = function() return "I am a source!" end},
   id = 1
 }
 function M:setup()
   local null = require("null-ls")
   null.setup({
-    sources = {
-      null.builtins.formatting.stylua,
-      null.builtins.completion.spell
-    }
+    sources = {null.builtins.formatting.stylua, null.builtins.completion.spell}
   })
 end
 
