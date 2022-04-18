02a04c580f8fdb9046ebac7a3ea30ffad32aab36
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2022-04-17 20:08:13 -0400

    feat(uml): transfer support to null-ls (work in progress)

--- defaults/.config/nvim/lua/config/plugins/null-ls.lua
+++ defaults/.config/nvim/lua/config/plugins/null-ls.lua
@@ -1,19 +1,43 @@
 local M = {}
 
 --- Dependencies:
---  
 M.plantuml = {
   name = "plantuml",
-  filetypes = {["plantuml"] = true},
+  filetypes = {"plantuml"},
   methods = {[require("null-ls").methods.DIAGNOSTICS] = true},
-  generator = {fn = function() return "I am a source!" end},
+  generator = require("null-ls").generator({
+    command = "plantuml",
+    args = {"$FILENAME"},
+    to_stdin = true,
+    from_stderr = true,
+    -- choose an output format (raw, json, or line)
+    format = "raw",
+    check_exit_code = function(code, stderr)
+      local success = code == 0
+
+      if not success then
+        -- can be noisy for things that run often (e.g. diagnostics), but can
+        -- be useful for things that run on demand (e.g. formatting)
+        print(stderr)
+      end
+
+      return success
+    end,
+    -- use helpers to parse the output from string matchers,
+    -- or parse it manually with a function
+    -- 'errorformat': '%EError line %l in file: %f,%Z%m',
+    on_output = require("null-ls.helpers").diagnostics.from_errorformat(
+        [[%EError line %l in file: %f,%Z%m]], 'plantuml')
+  }),
   id = 1
 }
 function M:setup()
   local null = require("null-ls")
   null.setup({
+    debug = true,
     sources = {null.builtins.formatting.stylua, null.builtins.completion.spell}
   })
+  null.register(self.plantuml)
 end
 
 return M
