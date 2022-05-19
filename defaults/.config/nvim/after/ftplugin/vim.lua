3aa0129a4389201dbe0e3720b7b2c340a993ec35
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2022-05-18 20:45:11 -0400

    refact: switching ftplugin to lua

---
+++ defaults/.config/nvim/after/ftplugin/vim.lua
@@ -0,0 +1,22 @@
+---@module vim after ftplugin
+---@author Reinaldo Molina
+
+if vim.b.did_vim_ftplugin then
+  return
+end
+
+vim.b.did_vim_ftplugin = 1
+
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.tabstop = 2
+
+vim.cmd[[
+  " Quote text by inserting "> "
+  nnoremap <buffer> <plug>make_file :so %<cr>
+  nnoremap <buffer> <plug>make_project :so %<cr>
+  " Echo highlighted text
+  vnoremap <buffer> <localleader>e y:echomsg <c-r>"<cr>
+  " Evaluate highlighted text
+  vnoremap <buffer> <localleader>E y:<c-r>"<cr>
+]]
