d1436d311b7a2193a66bb5f0007fc7078d3c8682
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2022-05-26 12:19:05 -0400

    feat: org after plugin baseline

---
+++ defaults/.config/nvim/after/ftplugin/org.lua
@@ -0,0 +1,27 @@
+---@module org after ftplugin
+---@author Reinaldo Molina
+
+if vim.b.did_org_ftplugin then
+  return
+end
+
+vim.b.did_org_ftplugin = 1
+
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.spell = true
+
+vim.b.AutoPairs = {
+  ['='] = '=',
+  ['('] = ')',
+  ['"'] = '"',
+  ['['] = ']',
+  ['<'] = '>',
+}
+
+if vim.fn.exists("*mdip#MarkdownClipboardImage") > 0 then
+  local opts = {silent = true, buffer = true, desc = "insert_clipboard_image"}
+  vim.keymap.set("n", "<localleader>i", 
+    function() return vim.fn["mdip#MarkdownClipboardImage"]() end, opts)
+end
