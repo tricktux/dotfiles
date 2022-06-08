645267b24f88b15a66fb023bd32971dcd3c61111
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2022-06-08 14:58:41 -0400

    fix(indent): seems to agree more with plugin

--- defaults/.config/nvim/after/ftplugin/org.lua
+++ defaults/.config/nvim/after/ftplugin/org.lua
@@ -7,9 +7,9 @@ end
 
 vim.b.did_org_ftplugin = 1
 
-vim.opt_local.shiftwidth = 2
-vim.opt_local.softtabstop = 2
-vim.opt_local.tabstop = 2
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.tabstop = 4
 vim.opt_local.spell = true
 
 vim.b.AutoPairs = {
