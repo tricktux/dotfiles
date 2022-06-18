a40a4dc4393ff0517ed910fdd9e1377925877bc3
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2022-06-18 00:34:49 -0400

    chore: address deprecated options

--- defaults/.config/nvim/lua/config/plugins/tree_explorer.lua
+++ defaults/.config/nvim/lua/config/plugins/tree_explorer.lua
@@ -5,23 +5,7 @@ local M = {}
 
 function M.nvimtree_config()
   -- These additional options must be set **BEFORE** calling `require'nvim-tree'` or calling setup.
-  vim.g.nvim_tree_git_hl = 0 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
-  vim.g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
-  vim.g.nvim_tree_root_folder_modifier = ':~' -- This is the default. See :help filename-modifiers for more options
   vim.g.nvim_tree_width_allow_resize = 1 -- 0 by default, will not resize the tree when opening a file
-  vim.g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
-  vim.g.nvim_tree_show_icons = {['git'] = 0, ['folders'] = 0, ['files'] = 0}
-  vim.g.nvim_tree_group_empty = 1 -- 0 by default, compact folders that only contain a single folder into one node in the file tree
-  -- one space by default, used for rendering the space between the icon and
-  -- the filename. Use with caution, it could break rendering if you set an
-  -- empty string depending on your font.
-  vim.g.nvim_tree_icon_padding = ' '
-  vim.g.nvim_tree_special_files = {
-    ['README.md'] = 1,
-    ['Makefile'] = 1,
-    ['MAKEFILE'] = 1
-  } -- List of filenames that gets highlighted with NvimTreeSpecialFile
-  vim.g.nvim_tree_respect_buf_cwd = 1
 
   local tree_cb = require("nvim-tree.config").nvim_tree_callback
 
