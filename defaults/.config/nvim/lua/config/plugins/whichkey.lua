4f6e86722338eb0bd1063afa08dfc4d61a55de63
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2022-07-20 00:01:55 -0400

    feat: prettier windows

--- defaults/.config/nvim/lua/config/plugins/whichkey.lua
+++ defaults/.config/nvim/lua/config/plugins/whichkey.lua
@@ -39,7 +39,7 @@ M.__config = {
     group = "+" -- symbol prepended to a group
   },
   window = {
-    border = "none", -- none, single, double, shadow
+    border = "single", -- none, single, double, shadow
     position = "bottom", -- bottom, top
     margin = {1, 0, 1, 0}, -- extra window margin [top, right, bottom, left]
     padding = {1, 2, 1, 2} -- extra window padding [top, right, bottom, left]
