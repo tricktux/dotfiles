986f8d5a65a5783663456b261532538e150ec502
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2022-07-28 11:09:37 -0400

    feat(exec): repl for current file

--- defaults/.config/nvim/after/ftplugin/python.lua
+++ defaults/.config/nvim/after/ftplugin/python.lua
@@ -6,6 +6,16 @@ if vim.b.did_python_ftplugin then
 end
 
 vim.b.did_python_ftplugin = 1
+local vks = vim.keymap.set
+local utl = require('utils.utils')
+local fn = vim.fn
 
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
+
+local o = {silent = true, buffer = true, desc = 'terminal_send_file'}
+local r = function()
+  local filename = fn.expand("%")
+  utl.term.exec(fmt("python %s", filename))
+end
+vks('n', '<plug>terminal_send_file', r, o)
