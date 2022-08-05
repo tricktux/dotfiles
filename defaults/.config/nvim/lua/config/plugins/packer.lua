b57113699976e434a54022a0fbc5079f2d45ca23
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2022-08-05 12:10:43 -0400

    fix(lsp): tag condition was not setup properly

--- defaults/.config/nvim/lua/config/plugins/packer.lua
+++ defaults/.config/nvim/lua/config/plugins/packer.lua
@@ -137,7 +137,7 @@ M.__plugins.common = {
 	},
 	{
 		"neovim/nvim-lspconfig",
-		tag = vim.fn.has("nvim-0.8") and "*" or "v0.1.3*", -- Compatible with 0.7.0
+		tag = vim.fn.has("nvim-0.8") > 0 and "*" or "v0.1.3*", -- Compatible with 0.7.0
 		requires = { { "ray-x/lsp_signature.nvim" }, { "j-hui/fidget.nvim" } },
 		config = function()
 			require("config.lsp").setup()
