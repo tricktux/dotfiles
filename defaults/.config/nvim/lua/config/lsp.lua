a69411ba90b5b6da4b21363a1e929c652f4a9b04
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2022-09-20 13:58:44 -0400

    perf: entire runtime is way too much, brings pc to a halt

--- defaults/.config/nvim/lua/config/lsp.lua
+++ defaults/.config/nvim/lua/config/lsp.lua
@@ -209,10 +209,10 @@ function M:config()
             -- Get the language server to recognize the `vim` global
             globals = {'vim'},
           },
-          workspace = {
+          -- workspace = {
             -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file("", true),
-          },
+            -- library = vim.api.nvim_get_runtime_file("", true),
+          -- },
           -- Do not send telemetry data containing a randomized but unique identifier
           telemetry = {
             enable = false,
