3ec7069709f82c3f88f84bc799cc684749da2177
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2022-09-26 13:02:37 -0400

    fix(lua): completion for runtime files

--- defaults/.config/nvim/lua/config/lsp.lua
+++ defaults/.config/nvim/lua/config/lsp.lua
@@ -212,10 +212,10 @@ function M:config()
 						-- Get the language server to recognize the `vim` global
 						globals = { "vim" },
 					},
-					-- workspace = {
+					workspace = {
 					-- Make the server aware of Neovim runtime files
-					-- library = vim.api.nvim_get_runtime_file("", true),
-					-- },
+            library = os.getenv("VIMRUNTIME"),
+					},
 					-- Do not send telemetry data containing a randomized but unique identifier
 					telemetry = {
 						enable = false,
