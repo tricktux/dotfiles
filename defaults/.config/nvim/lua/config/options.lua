efe855adb41b04e69bc72ffcedd2c09cc75928b1
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2022-11-13 15:01:04 -0500

    feat(windows): adding support for powershell

--- defaults/.config/nvim/lua/config/options.lua
+++ defaults/.config/nvim/lua/config/options.lua
@@ -47,6 +47,14 @@ function M:setup()
   -- s - Do not show search hit bottom
   -- t - Truncate message if is too long
   vim.opt.shortmess = "aoOcst"
+  if vim.fn.has("unix") <= 0 then
+    vim.opt.shell = "powershell"
+    vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+    vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+    vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+    vim.opt.shellquote = "" 
+    vim.opt.shellxquote = ""
+  end
 end
 
 return M
