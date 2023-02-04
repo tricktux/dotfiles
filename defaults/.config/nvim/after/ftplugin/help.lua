de33adbc483b17f675eb19914351a93089367977
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-02-04 08:36:42 -0500

    fix: restore navigation between help files

--- defaults/.config/nvim/after/ftplugin/help.lua
+++ defaults/.config/nvim/after/ftplugin/help.lua
@@ -13,3 +13,4 @@ local opts = {silent = true, buffer = true, desc = 'exit help'}
 vim.keymap.set('n', [[q]], [[:helpc<cr>]], opts)
 opts.desc = 'show sections'
 vim.keymap.set('n', [[g0]], [[g0]], opts)
+vim.keymap.set('n', [[K]], [[K]], opts)
