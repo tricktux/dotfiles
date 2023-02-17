c8e5c40b02f717d0a58dde4d7539436c4e0871a6
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-02-16 22:25:54 -0500

    chore: remove deprecated code

--- defaults/.config/nvim/lua/aucmds.lua
+++ defaults/.config/nvim/lua/aucmds.lua
@@ -1,31 +1,7 @@
 local log = require("utils.log")
 local api = vim.api
 
-local function create(definitions)
-  for group_name, definition in pairs(definitions) do
-    log.info("Setting augroup = ", group_name)
-    vim.cmd("augroup " .. group_name)
-    vim.cmd("autocmd!")
-    for _, def in ipairs(definition) do
-      local command = table.concat(vim.tbl_flatten({ "autocmd", def }), " ")
-      log.info(command)
-      vim.cmd(command)
-    end
-    vim.cmd("augroup END")
-  end
-end
-
-local function deprecated()
-  create({
-    highlight_yank = {
-      {
-        "TextYankPost",
-        "*",
-        [[silent! lua require'vim.highlight'.on_yank{timeout = 250}]],
-      },
-    },
-  })
-end
+local M = {}
 
 local function set_text_settings()
   vim.opt.conceallevel = 0
@@ -37,12 +13,7 @@ local function set_text_settings()
   vim.opt.comments:append({ b = "-" })
 end
 
-local function setup()
-  if vim.fn.has("nvim-0.7") <= 0 then
-    deprecated()
-    return
-  end
-
+M.setup = function()
   local id = api.nvim_create_augroup("highlight_yank", { clear = true })
   api.nvim_create_autocmd("TextYankPost", {
     callback = function()
@@ -163,4 +134,4 @@ local function setup()
   end
 end
 
-return { create = create, setup = setup }
+return M
