74054c41d9de2e69063fd2fbc5f2d5a9d3306eb4
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-02-17 12:09:32 -0500

    fix(window): avoid using alt for window navigation
    - When using docker or ssh or something else this combined mappings from
    kitty to nvim are not working
    - Moved to using alt on neovim
    - kitty_mod for moving between windows
    - and alt arrows for moving tabs
    - I like it right now
    - And it does not interfere with other stuff

--- defaults/.config/nvim/lua/plugins/unix.lua
+++ defaults/.config/nvim/lua/plugins/unix.lua
@@ -9,19 +9,6 @@ return {
   { "neomutt/neomutt.vim", ft = "muttrc" },
   { "fladson/vim-kitty", ft = "kitty" },
   {
-    "knubie/vim-kitty-navigator",
-    build = "cp ./*.py ~/.config/kitty/",
-    init = function()
-      vim.g.kitty_navigator_no_mappings = 1
-    end,
-    keys = {
-      { "<a-h>", "<cmd>KittyNavigateLeft<cr>", desc = "kitty_left" },
-      { "<a-j>", "<cmd>KittyNavigateDown<cr>", desc = "kitty_down" },
-      { "<a-k>", "<cmd>KittyNavigateUp<cr>", desc = "kitty_up" },
-      { "<a-l>", "<cmd>KittyNavigateRight<cr>", desc = "kitty_right" },
-    },
-  },
-  {
     "untitled-ai/jupyter_ascending.vim",
     ft = "python",
     init = function()
