78caecbd01f4a5f5ab0b04dc8a30a01400f3f96b
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-02-16 23:26:26 -0500

    feat: stole lazyvim icons

--- defaults/.config/nvim/lua/utils/utils.lua
+++ defaults/.config/nvim/lua/utils/utils.lua
@@ -6,12 +6,62 @@ local fn = vim.fn
 
 local M = {}
 
+M.icons = {
+  diagnostics = {
+    Error = " ",
+    Warn = " ",
+    Hint = " ",
+    Info = " ",
+  },
+  git = {
+    added = " ",
+    modified = " ",
+    removed = " ",
+  },
+  kinds = {
+    Array = " ",
+    Boolean = " ",
+    Class = " ",
+    Color = " ",
+    Constant = " ",
+    Constructor = " ",
+    Copilot = " ",
+    Enum = " ",
+    EnumMember = " ",
+    Event = " ",
+    Field = " ",
+    File = " ",
+    Folder = " ",
+    Function = " ",
+    Interface = " ",
+    Key = " ",
+    Keyword = " ",
+    Method = " ",
+    Module = " ",
+    Namespace = " ",
+    Null = "ﳠ ",
+    Number = " ",
+    Object = " ",
+    Operator = " ",
+    Package = " ",
+    Property = " ",
+    Reference = " ",
+    Snippet = " ",
+    String = " ",
+    Struct = " ",
+    Text = " ",
+    TypeParameter = " ",
+    Unit = " ",
+    Value = " ",
+    Variable = " ",
+  },
+}
+
 -- List of useful vim helper functions
 -- >vim.tbl_*
 --  >vim.tbl_count
 -- >vim.validate
 -- >vim.deepcopy
-
 M.set = {}
 function M.set.new(t)
 	vim.validate({ t = { t, "t" } })
