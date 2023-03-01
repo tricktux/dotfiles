f10502fbba2775eb657cf38acea1b130ad795c6a
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-03-01 05:01:25 -0500

    chore: formatting

--- defaults/.config/nvim/after/ftplugin/c.lua
+++ defaults/.config/nvim/after/ftplugin/c.lua
@@ -8,27 +8,27 @@ vim.b.did_cpp_ftplugin = 1
 local fmt = string.format
 local fn = vim.fn
 local vks = vim.keymap.set
-local log = require('utils.log')
-local u = require('utils.utils')
+local log = require("utils.log")
+local u = require("utils.utils")
 
 local function repl()
   local comp = nil
-  if fn.executable('clang++') > 0 then
-    comp = 'clang++'
-  elseif fn.executable('g++') > 0 then
-    comp = 'g++'
+  if fn.executable("clang++") > 0 then
+    comp = "clang++"
+  elseif fn.executable("g++") > 0 then
+    comp = "g++"
   else
     vim.notify("[cpp.repl]: no compiler available", vim.log.levels.ERROR)
     return
   end
 
   local out = fn.tempname()
-  local exec_out = fn.has('unix') > 0 and out or out .. '.exe'
+  local exec_out = fn.has("unix") > 0 and out or out .. ".exe"
   local filename = fn.expand("%")
   local cmd = fn.shellescape(fmt("%s %s -g -O3 -o %s && %s", comp, filename, out, exec_out))
   log.info(fmt("repl.cpp.cmd = %s", cmd))
   u.term.exec(cmd)
 end
 
-local o = {desc = 'terminal_send_file'}
-vks('n', '<plug>terminal_send_file', repl, o)
+local o = { desc = "terminal_send_file" }
+vks("n", "<plug>terminal_send_file", repl, o)
