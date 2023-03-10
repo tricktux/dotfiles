098abb47a089384fd3c60d76f5f78e37a96b35fa
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-03-10 18:08:49 -0500

    fix(gpt): give me more available tokens

--- defaults/.config/nvim/lua/plugins/init.lua
+++ defaults/.config/nvim/lua/plugins/init.lua
@@ -805,7 +805,17 @@ return {
 	{
 		"dpayne/CodeGPT.nvim",
 		cmd = { "Chat" },
-		opts = {},
+		opts = {
+			openai_params = {
+				model = "gpt-3.5-turbo",
+				frequency_penalty = 0,
+				presence_penalty = 0,
+				max_tokens = 1024,
+				temperature = 0,
+				top_p = 1,
+				n = 1,
+			},
+		},
 		dependencies = {
 			"MunifTanjim/nui.nvim",
 			"nvim-lua/plenary.nvim",
