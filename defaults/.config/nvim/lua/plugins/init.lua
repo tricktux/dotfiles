f7ed53873a86b8539855f5f84a5b96a097668a7e
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-03-26 12:23:28 -0400

    feat(gpt): more tokens

--- defaults/.config/nvim/lua/plugins/init.lua
+++ defaults/.config/nvim/lua/plugins/init.lua
@@ -810,7 +810,7 @@ return {
 				model = "gpt-3.5-turbo",
 				frequency_penalty = 0,
 				presence_penalty = 0,
-				max_tokens = 1024,
+				max_tokens = 3000,
 				temperature = 0,
 				top_p = 1,
 				n = 1,
