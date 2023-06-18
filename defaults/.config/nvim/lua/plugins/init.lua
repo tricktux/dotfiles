939eda5cf69d004f89cf1e38c93a402674404466
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-06-18 18:14:13 -0400

    chore: address deprication

--- defaults/.config/nvim/lua/plugins/init.lua
+++ defaults/.config/nvim/lua/plugins/init.lua
@@ -656,7 +656,7 @@ return {
     init = function()
       vim.g.lazygit_floating_window_winblend = 0                          -- transparency of floating window
       vim.g.lazygit_floating_window_scaling_factor = 0.9                  -- scaling factor for floating window
-      vim.g.lazygit_floating_window_corner_chars = { "╭", "╮", "╰", "╯" } -- customize lazygit popup window corner characters
+      vim.g.lazygit_floating_window_border_chars = { "╭", "╮", "╰", "╯" } -- customize lazygit popup window corner characters
       vim.g.lazygit_use_neovim_remote = 0
     end,
   },
