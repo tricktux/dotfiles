ec256cae2aaff8cbe575e865112e328bcdda5654
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-06-18 18:14:34 -0400

    chore: formatting

--- defaults/.config/nvim/lua/plugins/init.lua
+++ defaults/.config/nvim/lua/plugins/init.lua
@@ -954,13 +954,13 @@ return {
         require("noice").redirect(vim.fn.getcmdline())
       end, { desc = "Redirect Cmdline" })
 
-      vim.keymap.set({"n", "i", "s"}, "<c-u>", function()
+      vim.keymap.set({ "n", "i", "s" }, "<c-u>", function()
         if not require("noice.lsp").scroll(4) then
           return "<c-f>"
         end
       end, { silent = true, expr = true })
 
-      vim.keymap.set({"n", "i", "s"}, "<c-d>", function()
+      vim.keymap.set({ "n", "i", "s" }, "<c-d>", function()
         if not require("noice.lsp").scroll(-4) then
           return "<c-b>"
         end
