964599574d67b627387fcd3e8eb96cbb414ea4c1
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-06-19 09:19:58 -0400

    fix(notify): remove it
    - Extremely distracting
    - Hard to read the messages
    - Just remove it

--- defaults/.config/nvim/lua/plugins/init.lua
+++ defaults/.config/nvim/lua/plugins/init.lua
@@ -131,52 +131,6 @@ return {
     },
   },
   {
-    -- TODO: move it to telescope.nvim
-    "rcarriga/nvim-notify",
-    event = "VeryLazy",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-      -- Minimum level to show
-      level = "info",
-
-      -- Animation style (see below for details)
-      stages = "fade_in_slide_out",
-
-      -- Function called when a new window is opened, use for changing win settings/config
-      on_open = nil,
-
-      -- Function called when a window is closed
-      on_close = nil,
-
-      -- Render function for notifications. See notify-render()
-      render = "default",
-
-      -- Default timeout for notifications
-      timeout = 500,
-
-      -- Max number of columns for messages
-      max_width = nil,
-      -- Max number of lines for a message
-      max_height = nil,
-
-      -- For stages that change opacity this is treated as the highlight behind the window
-      -- Set this to either a highlight group, an RGB hex value e.g. "#000000" or a function returning an RGB code for dynamic values
-      background_colour = "#000000",
-
-      -- Minimum width for notification windows
-      minimum_width = 50,
-
-      -- Icons for the different levels
-      icons = {
-        ERROR = "",
-        WARN = "",
-        INFO = "",
-        DEBUG = "",
-        TRACE = "✎",
-      },
-    },
-  },
-  {
     -- TODO: move it to treesitter.nvim
     "danymat/neogen",
     keys = {
@@ -991,7 +945,8 @@ return {
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
+      -- I find this plugin really distracting
+      -- "rcarriga/nvim-notify",
     }
   }
 }
