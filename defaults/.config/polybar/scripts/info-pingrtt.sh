015bf55d687c5e89eaf3f69a254fbb05c89d3541
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-06-21 18:54:29 -0400

    Revert "fix(ping): internet speed too long and time/power consuming, reverting"
    This reverts commit 609c615e844114adb67536e9ef61e9f26dccafac.

--- defaults/.config/polybar/scripts/info-pingrtt.sh
+++ defaults/.config/polybar/scripts/info-pingrtt.sh
@@ -2,17 +2,21 @@
 
 HOST=1.1.1.1
 
-ping -n -c 1 -W $HOST > /dev/null 2>&1 || exit
-
-ping=ping -n -c 1 -W $HOST
-rtt=$(echo "$ping" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
-
-if [ "$rtt" -lt 50 ]; then
-	icon="%{F#3cb703}%{F-}"
-elif [ "$rtt" -lt 150 ]; then
-	icon="%{F#f9dd04}%{F-}"
+if ! ping=$(speedtest --simple 2> /dev/null); then
+    rtt="--"
+    icon="%{F#d60606}%{F-}"
 else
-	icon="%{F#d60606}%{F-}"
-fi
+    rtt=$(echo "$ping" | sed -rn 's/.*Ping: ([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
+    down=$(echo "$ping" | sed -rn 's/.*Download: ([0-9]{1,})\.?[0-9]{0,} Mbit\/s.*/\1/p')
+    up=$(echo "$ping" | sed -rn 's/.*Upload: ([0-9]{1,})\.?[0-9]{0,} Mbit\/s.*/\1/p')
+    out="$rtt ms  $down Mbit/s  $up Mbit/s"
 
-echo " $icon $rtt ms "
+    if [ "$rtt" -lt 50 ]; then
+        icon="%{F#3cb703}%{F-}"
+    elif [ "$rtt" -lt 150 ]; then
+        icon="%{F#f9dd04}%{F-}"
+    else
+        icon="%{F#d60606}%{F-}"
+    fi
+fi
+echo "$icon $out"
