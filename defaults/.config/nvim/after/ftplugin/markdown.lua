0b9b95dbf3ebfe2232ae1fca56c92d9649949fd5
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-07-08 12:07:26 -0400

    feat(markdown): replace the vim version with the lua one
    - There was too much legacy content there
    - Use a more strip down version
    - Add stuff as needed
    - Left the file around in case needed

---
+++ defaults/.config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,35 @@
+---@module markdown after ftplugin
+---@author Reinaldo Molina
+
+if vim.b.did_markdown_ftplugin then
+  return
+end
+
+vim.b.did_markdown_ftplugin = 1
+
+local u = require("utils.utils")
+local preview = function()
+  local o = vim.fn.expand("%:r") .. ".html"
+  u.term.firefox_preview_async(o)
+end
+
+local _filetype = { "html", "docx", "pdf" }
+local function _validate_filetypes(filetype)
+  return vim.tbl_contains(_filetype, filetype)
+end
+
+local render = function(filetype)
+  vim.validate({ filetype = { filetype, _validate_filetypes, "one of: " .. vim.inspect(_filetype) } })
+  local f = vim.fn.shellescape(vim.fn.expand("%:p"))
+  local o = vim.fn.shellescape(vim.fn.expand("%:r") .. "." .. filetype)
+  local cmd = { "!pandoc", f, "-o", o }
+  vim.cmd(table.concat(cmd, " "))
+end
+
+if vim.g.no_plugin_maps == nil and vim.g.no_markdown_maps == nil then
+  local vks = vim.keymap.set
+		vks("n", "<plug>make_file", function()
+			render("html")
+		end, { desc = "pandoc-render-html" })
+  vks("n", "<plug>preview", preview, { desc = "file_preview" })
+end
