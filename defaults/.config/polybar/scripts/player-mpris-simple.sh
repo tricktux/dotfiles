5d60ad7c6e4d3331d8664e2dc25c9f859f98f576
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-07-23 15:00:27 -0400

    fix: showing no player error message

--- defaults/.config/polybar/scripts/player-mpris-simple.sh
+++ defaults/.config/polybar/scripts/player-mpris-simple.sh
@@ -3,9 +3,9 @@
 player_status=$(playerctl status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-    echo " $(playerctl metadata artist) - $(playerctl metadata title)"
+    echo " $(playerctl --no-messages metadata artist) - $(playerctl --no-messages metadata title)"
 elif [ "$player_status" = "Paused" ]; then
-    echo " $(playerctl metadata artist) - $(playerctl metadata title)"
+    echo " $(playerctl --no-messages metadata artist) - $(playerctl --no-messages metadata title)"
 # else
     # echo "#3"
 fi
