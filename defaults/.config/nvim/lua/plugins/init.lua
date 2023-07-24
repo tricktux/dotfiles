a42852d55cbcd5fd71dab9534523885366ef1493
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-07-24 14:44:17 -0400

    feat(oil): add <bs> as previous folder

--- defaults/.config/nvim/lua/plugins/init.lua
+++ defaults/.config/nvim/lua/plugins/init.lua
@@ -860,42 +860,60 @@ return {
 				win_options = {
 					winblend = 0,
 				},
-			},
-			view_options = {
-				-- Show files and directories that start with "."
-				show_hidden = true,
-				-- This function defines what is considered a "hidden" file
-				is_hidden_file = function(name, bufnr)
-					return vim.startswith(name, ".")
-				end,
-				-- This function defines what will never be shown, even when `show_hidden` is set
-				is_always_hidden = function(name, bufnr)
-					return false
-				end,
-			},
-		},
-	},
-	{
-		"cshuaimin/ssr.nvim",
-		keys = {
-			{
-				"<leader>ss",
-				function()
-					require("ssr").open()
-				end,
-				mode = { "n", "v" },
-				desc = "structured-search-replace",
-			},
-		},
-	},
-	{
-		"luukvbaal/statuscol.nvim",
-		event = "VeryLazy",
-	},
-	{
-		"williamboman/mason.nvim",
-		event = "VeryLazy",
-		build = ":MasonUpdate",
-		opts = {},
-	},
+      },
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = "actions.select_vsplit",
+        ["<C-v>"] = "actions.select_vsplit",
+        ["<C-h>"] = "actions.select_split",
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["q"] = "actions.close",
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["<bs>"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["g."] = "actions.toggle_hidden",
+      },
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+        -- This function defines what is considered a "hidden" file
+        is_hidden_file = function(name, bufnr)
+          return vim.startswith(name, ".")
+        end,
+        -- This function defines what will never be shown, even when `show_hidden` is set
+        is_always_hidden = function(name, bufnr)
+          return false
+        end,
+      },
+    },
+  },
+  {
+    "cshuaimin/ssr.nvim",
+    keys = {
+      {
+        "<leader>ss",
+        function()
+          require("ssr").open()
+        end,
+        mode = { "n", "v" },
+        desc = "structured-search-replace",
+      },
+    },
+  },
+  {
+    "luukvbaal/statuscol.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "williamboman/mason.nvim",
+    event = "VeryLazy",
+    build = ":MasonUpdate",
+    opts = {},
+  },
 }
