28897d04125f5b24c1b0d9e2fd0d97fb373aab95
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-07-27 15:51:45 -0400

    fix(focus): update options

--- defaults/.config/nvim/lua/plugins/init.lua
+++ defaults/.config/nvim/lua/plugins/init.lua
@@ -272,23 +272,54 @@ return {
 					desc = "window_switch_right",
 				},
 			},
+      init = function()
+        local ignore_filetypes = { 'neo-tree' }
+        local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+
+        local augroup = vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+        vim.api.nvim_create_autocmd('WinEnter', {
+            group = augroup,
+            callback = function(_)
+                if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+                    vim.b.focus_disable = true
+                end
+            end,
+            desc = 'Disable focus autoresize for BufType',
+        })
+        vim.api.nvim_create_autocmd('FileType', {
+            group = augroup,
+            callback = function(_)
+                if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+                    vim.b.focus_disable = true
+                end
+            end,
+            desc = 'Disable focus autoresize for FileType',
+        })
+      end,
 			opts = {
-				-- Displays line numbers in the focussed window only
-				-- Not displayed in unfocussed windows
-				-- Default: true
-				number = false,
-				excluded_buftypes = { "nofile", "prompt" },
-				relativenumber = false,
-				cursorline = false,
-				signcolumn = false,
-				-- Enable auto highlighting for focussed/unfocussed windows
-				-- Default: false
-				winhighlight = false,
-				-- vim.cmd('hi link UnfocusedWindow CursorLine')
-				-- vim.cmd('hi link FocusedWindow VisualNOS')
-				-- focus.enable = false
-				-- width = 100
-				tmux = true,
+        autoresize = {
+          enable = true, -- Enable or disable auto-resizing of splits
+          width = 0, -- Force width for the focused window
+          height = 0, -- Force height for the focused window
+          minwidth = 0, -- Force minimum width for the unfocused window
+          minheight = 0, -- Force minimum height for the unfocused window
+          height_quickfix = 10, -- Set the height of quickfix panel
+        },
+        ui = {
+            number = false, -- Display line numbers in the focussed window only
+            relativenumber = false, -- Display relative line numbers in the focussed window only
+            hybridnumber = false, -- Display hybrid line numbers in the focussed window only
+            absolutenumber_unfocussed = false, -- Preserve absolute numbers in the unfocussed windows
+
+            cursorline = false, -- Display a cursorline in the focussed window only
+            cursorcolumn = false, -- Display cursorcolumn in the focussed window only
+            colorcolumn = {
+                enable = false, -- Display colorcolumn in the foccused window only
+                list = '+1', -- Set the comma-saperated list for the colorcolumn
+            },
+            signcolumn = true, -- Display signcolumn in the focussed window only
+            winhighlight = false, -- Auto highlighting for focussed/unfocussed windows
+        }
 			},
 		},
 	},
@@ -916,7 +947,7 @@ return {
     build = ":MasonUpdate",
     opts = {},
   },
-  { 
+  {
     'simrat39/symbols-outline.nvim',
     cmd = "SymbolsOutline",
     keys = {
