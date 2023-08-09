d478e8ea3adf975ee83d6de324a21ec32cbe404f
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-08-09 10:24:38 -0400

    feat: add baseline support for zen-mode
    - AKA focus mode,
    - Which the window plugin took the name from

--- defaults/.config/nvim/lua/plugins/init.lua
+++ defaults/.config/nvim/lua/plugins/init.lua
@@ -964,4 +964,47 @@ return {
 		event = { "LspAttach" },
 		opts = {},
 	},
+	{
+		"folke/zen-mode.nvim",
+		cmd = "ZenMode",
+		keys = {
+			{ "<leader>tz", "<cmd>ZenMode<cr>", desc = "zen-mode" },
+		},
+		opts = {
+			plugins = {
+				-- disable some global vim options (vim.o...)
+				-- comment the lines to not apply the options
+				options = {
+					enabled = true,
+					ruler = false, -- disables the ruler text in the cmd line area
+					showcmd = false, -- disables the command in the last line of the screen
+				},
+				twilight = { enabled = false }, -- enable to start Twilight when zen mode opens
+				gitsigns = { enabled = false }, -- disables git signs
+				tmux = { enabled = false }, -- disables the tmux statusline
+				-- this will change the font size on kitty when in zen mode
+				-- to make this work, you need to set the following kitty options:
+				-- - allow_remote_control socket-only
+				-- - listen_on unix:/tmp/kitty
+				kitty = {
+					enabled = true,
+					font = "+4", -- font size increment
+				},
+				-- this will change the font size on alacritty when in zen mode
+				-- requires  Alacritty Version 0.10.0 or higher
+				-- uses `alacritty msg` subcommand to change font size
+				alacritty = {
+					enabled = false,
+					font = "14", -- font size
+				},
+				-- this will change the font size on wezterm when in zen mode
+				-- See alse also the Plugins/Wezterm section in this projects README
+				wezterm = {
+					enabled = false,
+					-- can be either an absolute font size or the number of incremental steps
+					font = "+4", -- (10% increase per step)
+				},
+			},
+		},
+	},
 }
