10248a862426ccd16a3551e49f77323dfe05b22c
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-08-09 15:26:02 -0400

    feat: find_image function tested

--- defaults/.config/nvim/lua/plugins/hologram.lua
+++ defaults/.config/nvim/lua/plugins/hologram.lua
@@ -13,44 +13,52 @@ M.sources = {
 
 M.image_type = "png"
 
-M.to_absolute = function(path)
-  -- vim.validate(path = { path = "s" })
-
- if string.sub(path, 0, 1) == "/" then
-    return path
-  end
-
-  if string.sub(path, 1, 2) == ":" then
-    return path
-  end
-
-  return vim.fs.join(vim.fn.expand("%:p:h"), path)
-end
-
 M.find_source = function(line)
-  -- vim.validate(line = { line = "s" })
+  vim.validate{ line = { line = "s" } }
+
+  local plok, pl = pcall(require, "plenary.path")
+  if not plok then
+    vim.api.nvim_err_writeln("Failed to load plenary.path")
+    return
+  end
+
+  local hlok, fs = pcall(require, "hologram.fs")
+  if not hlok then
+    vim.api.nvim_err_writeln("Failed to load hologram.fs")
+    return
+  end
 
   local _, si, _ = string.find(string.lower(line), M.image_type)
   if not si then
     return nil
   end
 
-  for _, source in ipairs(M.sources) do
-    for _, pattern in ipairs(source) do
+  for _, source in pairs(M.sources) do
+    for _, pattern in pairs(source) do
       local inline_link = string.match(line, pattern[1])
       if inline_link then
-        local path = string.match(inline_link, pattern[2])
+        local path = pl:new(string.match(inline_link, pattern[2]))
         -- Check if we have a root path in a cross platform way
-        path = M.to_absolute(path)
-        if fs.check_sig_PNG(path) then
-          return path
-        else
-          return nil
+        if path:exists() and fs.check_sig_PNG(path:absolute()) then
+          return path:absolute()
         end
       end
     end
   end
 
+  return nil
+end
+
+M.find_source_in_buffer = function()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+  local source = nil
+  for _, line in ipairs(lines) do
+    source = M.find_source(line)
+    if source then
+      return source
+    end
+  end
   return nil
 end
 
