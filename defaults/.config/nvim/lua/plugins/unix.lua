d97699fcd16de281cb8457fe86e7ff83be5e79dd
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-09-18 15:59:36 -0400

    chore: formatting

--- defaults/.config/nvim/lua/plugins/unix.lua
+++ defaults/.config/nvim/lua/plugins/unix.lua
@@ -1,43 +1,43 @@
 if vim.fn.has("win32") > 0 then
-	return {}
+  return {}
 end
 
 return {
-	{ "mboughaba/i3config.vim", ft = "i3config" },
-	{ "lambdalisue/suda.vim", cmd = { "SudaWrite", "SudaRead" } },
-	{ "chr4/nginx.vim", ft = "nginx" },
-	{ "neomutt/neomutt.vim", ft = "muttrc" },
-	{ "fladson/vim-kitty", ft = "kitty" },
-	{
-		"kevinhwang91/rnvimr",
-		keys = {
-			{ "<plug>file_ranger_browser", "<cmd>RnvimrToggle<cr>", desc = "file-ranger-browser" },
-		},
-	},
-	{
-		"untitled-ai/jupyter_ascending.vim",
-		ft = "python",
-		init = function()
-			vim.g.jupyter_ascending_default_mappings = false
-			vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-				pattern = "*.sync.py",
-				callback = function()
-					if vim.b.has_jupyter_plugin == true then
-						return
-					end
-					vim.b.has_jupyter_plugin = true
-					local opts = { silent = true, buffer = true, desc = "jupyter_execute" }
-					vim.keymap.set("n", "<localleader>j", "<Plug>JupyterExecute", opts)
-					opts.desc = "jupyter_execute_all"
-					vim.keymap.set("n", "<localleader>k", "<Plug>JupyterExecuteAll", opts)
-				end,
-			})
-		end,
-	},
-	{
-		"giusgad/pets.nvim",
-		opts = {},
-		cmd = { "PetsNewCustom", "PetsNew" },
-		dependencies = { "MunifTanjim/nui.nvim"},
-	},
+  { "mboughaba/i3config.vim", ft = "i3config" },
+  { "lambdalisue/suda.vim", cmd = { "SudaWrite", "SudaRead" } },
+  { "chr4/nginx.vim", ft = "nginx" },
+  { "neomutt/neomutt.vim", ft = "muttrc" },
+  { "fladson/vim-kitty", ft = "kitty" },
+  {
+    "kevinhwang91/rnvimr",
+    keys = {
+      { "<plug>file_ranger_browser", "<cmd>RnvimrToggle<cr>", desc = "file-ranger-browser" },
+    },
+  },
+  {
+    "untitled-ai/jupyter_ascending.vim",
+    ft = "python",
+    init = function()
+      vim.g.jupyter_ascending_default_mappings = false
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+        pattern = "*.sync.py",
+        callback = function()
+          if vim.b.has_jupyter_plugin == true then
+            return
+          end
+          vim.b.has_jupyter_plugin = true
+          local opts = { silent = true, buffer = true, desc = "jupyter_execute" }
+          vim.keymap.set("n", "<localleader>j", "<Plug>JupyterExecute", opts)
+          opts.desc = "jupyter_execute_all"
+          vim.keymap.set("n", "<localleader>k", "<Plug>JupyterExecuteAll", opts)
+        end,
+      })
+    end,
+  },
+  {
+    "giusgad/pets.nvim",
+    opts = {},
+    cmd = { "PetsNewCustom", "PetsNew" },
+    dependencies = { "MunifTanjim/nui.nvim" },
+  },
 }
