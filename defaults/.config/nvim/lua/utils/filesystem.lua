e4d79760c71485618b8359c11e71d59a628fd9db
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-09-25 16:16:53 -0400

    fix(fs): new is_path
    - Major refactor
    - Truly and nicely find relative paths
    - Remove duplicate regexes
    - Overall really nice

--- defaults/.config/nvim/lua/utils/filesystem.lua
+++ defaults/.config/nvim/lua/utils/filesystem.lua
@@ -8,74 +8,82 @@ M.sources = {
 		"!%[%[(.-)%]%]",
 	},
 	org = {
-    "%[%[(.-)%]%]",
-    "%[%[(.-)%]%[.-%]%]",
-    "%[%[file: (.-)%]%[.-%]%]",
+		"%[%[(.-)%]%]",
+		"%[%[(.-)%]%[.-%]%]",
+		"%[%[file: (.-)%]%[.-%]%]",
 	},
 }
 
 M.find_source = function(line, sources)
-	vim.validate({ line = { line, "s" }, sources = { sources, "t" } })
-
-	local plok, pl = pcall(require, "plenary.path")
-	if not plok then
-		vim.api.nvim_err_writeln("Failed to load plenary.path")
-		return
-	end
-
-	local check_file = function(path)
-		return path:exists() and path:is_file()
-	end
+	vim.validate({ line = { line, "s", false }, sources = { sources, "t", false } })
 
 	local ft = vim.api.nvim_buf_get_option(0, "filetype")
 	if M.sources[ft] == nil then
-		vim.api.nvim_err_writeln("No sources for filetype: " .. ft)
+		-- vim.api.nvim_err_writeln("No sources for filetype: " .. ft)
 		return nil
 	end
 
-  -- vim.print("Filetype: " .. ft)
+	-- vim.print("Filetype: " .. ft)
 	for _, pattern in pairs(M.sources[ft]) do
-    -- vim.print("Pattern: " .. pattern)
+		-- vim.print("Pattern: " .. pattern)
 		local match = string.match(line, pattern)
-    if match then
-      local path = pl:new(match)
-      if check_file(path) then
-        vim.print("File: '" .. path:absolute() .. "' found")
-        return path:absolute()
-      end
-      -- Make another attempt by constructing a full path
-      local new_path = pl:new(vim.fn.expand("%:p:h"), match)
-      if check_file(new_path) then
-        vim.print("File: '" .. new_path:absolute() .. "' found")
-        return new_path:absolute()
-      end
-      vim.api.nvim_err_writeln("Match found, but file does not exists: '" .. match .. "'")
-      return nil
-    end
+		if match then
+			return match
+		end
 	end
 
-  -- vim.print("No image pattern matched")
+	-- vim.print("No image pattern matched")
+	return nil
+end
+
+-- Path is expected to be plenary:Path
+local is_full_path = function(path)
+	vim.validate({ path = { path, "t", false } })
+	return path:is_absolute() and path:exists()
+end
+
+local is_path = function(path)
+	vim.validate({ path = { path, "s", false } })
+	local p = require("plenary.path")
+	local o = p:new(path)
+	if is_full_path(o) then
+		return o:absolute()
+	end
+	-- vim.print("its not full path")
+	-- Maybe it's relative to the file dir
+	local f = p:new(vim.fn.expand("%:p:h"), path)
+	if is_full_path(f) then
+		return f:absolute()
+	end
+	-- vim.print("it's not relative to file")
+	-- Maybe it's relative to the cwd dir
+	local c = p:new(vim.uv.cwd(), path)
+	if is_full_path(c) then
+		return c:absolute()
+	end
+	-- vim.print("it's not relative to cwd")
+	-- Maybe it has variables
+	local e = p:new(o:expand())
+	if is_full_path(e) then
+		return e:absolute()
+	end
+	-- vim.print("its not expanded")
+	-- We are done
 	return nil
 end
 
 M.open_source_in_line = function(line)
 	vim.validate({ line = { line, "s", false } })
-  -- vim.print("Line: " .. line)
+	-- vim.print("Line: " .. line)
 	local source = M.find_source(line, M.sources)
-	-- vim.print("Source: " .. source and nil "" )
-	if source ~= nil then
-		utl.term.open_file(source)
+	local u = source or line
+	local file = is_path(u)
+	if file then
+		utl.term.open_uri(file)
 		return
 	end
 
-	local plok, pl = pcall(require, "plenary.path")
-	if not plok then
-		vim.api.nvim_err_writeln("Failed to load plenary.path")
-		return
-	end
-	source = pl:new(line):absolute()
-	-- vim.print("Source not found, using: " .. vim.inspect(source))
-	utl.term.open_file(source)
+	utl.term.open_uri(u)
 end
 
 M.search_for_first_visible_source = function(sources)
