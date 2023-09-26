bf8ac5972ecdec6b3dbacec95d1938326fdb287b
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-09-25 21:50:09 -0400

    feat: added yank mapping

--- defaults/.config/nvim/lua/plugins/oil.lua
+++ defaults/.config/nvim/lua/plugins/oil.lua
@@ -1,25 +1,46 @@
 local utl = require("utils.utils")
+local fs = require("utils.filesystem")
+
+local oil_get_entry = function()
+  local o = require("oil")
+  local d = fs.is_path(o.get_current_dir())
+  if d == nil then
+    vim.api.nvim_err_writeln("No oil directory found")
+    return nil
+  end
+  local e = o.get_entry_on_line(0, vim.fn.line("."))
+  if e == nil then
+    vim.api.nvim_err_writeln("No oil file found")
+    return nil
+  end
+  local f = fs.is_path(vim.fs.joinpath(d, e.name))
+  if f == nil then
+    vim.api.nvim_err_writeln("File: '" .. f .. "' not found")
+    return nil
+  end
+
+  return f
+end
+
+local oil_yank_entry = function()
+  local e = oil_get_entry()
+  if e == nil then
+    return
+  end
+
+  vim.fn.setreg('"', e)
+  vim.fn.setreg('+', e)
+  vim.fn.setreg('*', e)
+  vim.notify("Yanked to clipboard: " .. e)
+end
 
 local oil_start_entry = function()
-	local o = require("oil")
-	local p = require("plenary.path")
-	local d = p:new(o.get_current_dir())
-	if d == nil then
-		vim.api.nvim_err_writeln("No oil directory found")
-		return
-	end
-	local e = o.get_entry_on_line(0, vim.fn.line("."))
-	if e == nil then
-		vim.api.nvim_err_writeln("No oil file found")
-		return
-	end
-	local f = d:joinpath(e.name)
-	if not f:exists() or not f:is_file() then
-		vim.api.nvim_err_writeln("File: '" .. f:absolute() .. "' not found")
-		return
-	end
+  local e = oil_get_entry()
+  if e == nil then
+    return
+  end
 
-	utl.term.open_uri(f:absolute())
+	utl.term.open_uri(e)
 end
 
 local nvim_open_win_override = function(conf)
@@ -28,7 +49,6 @@ local nvim_open_win_override = function(conf)
 	local e = lines.end_line
 	conf.height = math.floor((e - s) * 0.8)
 	conf.row = math.floor((e - s) * 0.1)
-	vim.print(vim.inspect(conf))
 end
 
 return {
@@ -47,9 +67,8 @@ return {
 			pattern = "oil",
 			callback = function(args)
 				vim.keymap.set("n", "q", "<cmd>normal! ZZ<cr>", { buffer = args.buf })
-				vim.keymap.set("n", "s", function()
-					oil_start_entry()
-				end, { buffer = args.buf })
+				vim.keymap.set("n", "s", oil_start_entry, { buffer = args.buf })
+        vim.keymap.set("n", "y", oil_yank_entry, { buffer = args.buf })
 			end,
 		})
 	end,
