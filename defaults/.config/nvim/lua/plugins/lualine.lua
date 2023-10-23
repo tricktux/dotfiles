02387fed7cb0ad9e2e3f2cb4c8fd97ca345c3154
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-10-23 17:29:06 -0400

    fix: remove duplicated tables and extra info
    - The diagnostics info, as well as git info is in the tabline
    - Removed it from there

--- defaults/.config/nvim/lua/plugins/lualine.lua
+++ defaults/.config/nvim/lua/plugins/lualine.lua
@@ -28,25 +28,8 @@ return {
 				lualine_a = { "mode" },
 				lualine_b = { "branch" },
 				lualine_c = {
-					{
-						"diagnostics",
-						symbols = {
-							error = icons.diagnostics.Error,
-							warn = icons.diagnostics.Warn,
-							info = icons.diagnostics.Info,
-							hint = icons.diagnostics.Hint,
-						},
-					},
 					{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
 					{ "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
-					{
-						function()
-							return require("nvim-navic").get_location()
-						end,
-						cond = function()
-							return package.loaded["nvim-navic"] and require("nvim-navic").is_available()
-						end,
-					},
 				},
 				lualine_x = {
 					{
@@ -54,49 +37,11 @@ return {
 						cond = require("lazy.status").has_updates,
 						color = fg("Special"),
 					},
-					{
-						"diff",
-						symbols = {
-							added = icons.git.added,
-							modified = icons.git.modified,
-							removed = icons.git.removed,
-						},
-					},
 				},
 				lualine_y = {
 					{ "progress", separator = "", padding = { left = 1, right = 0 } },
 					{ "location", padding = { left = 0, right = 1 } },
 				},
-				lualine_z = {
-					function()
-						return " " .. os.date("%R")
-					end,
-				},
-			},
-			extensions = { "neo-tree" },
-			tabline = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = {},
-				lualine_y = {},
-				lualine_z = { "tabs" },
-			},
-			winbar = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = {},
-				lualine_y = {},
-				lualine_z = {},
-			},
-			inactive_winbar = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = {},
-				lualine_y = {},
-				lualine_z = {},
 			},
 			tabline = {
 				lualine_a = {
