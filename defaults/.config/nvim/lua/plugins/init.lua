5266cc0a7b1bb28f6b6d8b60211b8ddb80830bcc
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-11-03 12:31:37 -0400

    feat(gpt): added cool prompt for anki

--- defaults/.config/nvim/lua/plugins/init.lua
+++ defaults/.config/nvim/lua/plugins/init.lua
@@ -4,6 +4,26 @@ local utl = require("utils.utils")
 local vks = vim.keymap.set
 local api = vim.api
 
+
+-- Lua functions that inserts a text and copies it to the clipboard
+local function anki_prompt()
+  local p = [[
+I want you to act as a professional Anki card creator, able to create Anki cards from the text I provide.
+Regarding the formulation of the card content, you stick to two principles: First, minimum information principle: The material you learn must be formulated in as simple way as it is only possible. Simplicity does not have to imply losing information and skipping the difficult part.b Second, optimize wording: The wording of your items must be optimized to make sure that in minimum time the right bulb in your brain lights up. This will reduce error rates, increase specificity, reduce response time, and help your concentration.
+The following is a model card-create template for you to study.
+Text: The characteristics of the Dead Sea: Salt lake located on the border between Israel and Jordan. Its shoreline is the lowest point on the Earth's surface, averaging 396 m below sea level. It is 74 km long. It is seven times as salty (30% by volume) as the ocean. Its density keeps swimmers afloat. Only simple organisms can live in its saline waters
+Create cards based on the above text as follows:
+Q: Where is the Dead Sea located? A: on the border between Israel and Jordan Q: What is the lowest point on the Earth's surface? A: The Dead Sea shoreline Q: What is the average level on which the Dead Sea is located? A: 400 meters (below sea level) Q: How long is the Dead Sea? A: 70 km Q: How much saltier is the Dead Sea as compared with the oceans? A: 7 times Q: What is the volume content of salt in the Dead Sea? A: 30% Q: Why can the Dead Sea keep swimmers afloat? A: due to high salt content Q: Why is the Dead Sea called Dead? A: because only simple organisms can live in it Q: Why only simple organisms can live in the Dead Sea? A: because of high salt content
+  ]]
+
+  -- Copy text to buffer
+  vim.fn.setreg("+", p)
+  -- Insert text in buffer
+  vim.cmd("normal! p\"+")
+end
+
+vim.api.nvim_create_user_command("UtilsChatGptAnkiPastePrompt", anki_prompt, {})
+
 local function set_colorscheme(period)
   local flavour = {
     day = "latte",
