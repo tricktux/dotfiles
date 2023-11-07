14b2398e923510df4453903a3235cef1f40b0b9c
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-11-06 23:03:14 -0500

    fix(gpt): improve on anki prompt

--- defaults/.config/nvim/lua/plugins/init.lua
+++ defaults/.config/nvim/lua/plugins/init.lua
@@ -14,6 +14,8 @@ The following is a model card-create template for you to study.
 Text: The characteristics of the Dead Sea: Salt lake located on the border between Israel and Jordan. Its shoreline is the lowest point on the Earth's surface, averaging 396 m below sea level. It is 74 km long. It is seven times as salty (30% by volume) as the ocean. Its density keeps swimmers afloat. Only simple organisms can live in its saline waters
 Create cards based on the above text as follows:
 Q: Where is the Dead Sea located? A: on the border between Israel and Jordan Q: What is the lowest point on the Earth's surface? A: The Dead Sea shoreline Q: What is the average level on which the Dead Sea is located? A: 400 meters (below sea level) Q: How long is the Dead Sea? A: 70 km Q: How much saltier is the Dead Sea as compared with the oceans? A: 7 times Q: What is the volume content of salt in the Dead Sea? A: 30% Q: Why can the Dead Sea keep swimmers afloat? A: due to high salt content Q: Why is the Dead Sea called Dead? A: because only simple organisms can live in it Q: Why only simple organisms can live in the Dead Sea? A: because of high salt content
+
+Please use anki cards as described above to explain the concept of:
   ]]
 
   -- Copy text to buffer
