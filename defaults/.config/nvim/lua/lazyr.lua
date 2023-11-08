ff407410fa3db5bbdaba681c32c227e74cad1a4f
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-11-08 12:14:57 -0500

    fix: change default colorscheme

--- defaults/.config/nvim/lua/lazyr.lua
+++ defaults/.config/nvim/lua/lazyr.lua
@@ -47,7 +47,7 @@ M.opts = {
     -- install missing plugins on startup. This doesn't increase startup time.
     missing = true,
     -- try to load one of these colorschemes when starting an installation during startup
-    colorscheme = { "habamax" },
+    colorscheme = { "shine" },
   },
   ui = {
     -- a number <1 is a percentage., >1 is a fixed size
