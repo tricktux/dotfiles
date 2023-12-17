ee003d330e6d07711f70d3c7042a7ae751f2f310
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2023-12-16 21:37:07 -0500

    fix!: typo that was mapping n
    - Just remove mappings for now
    - Dap is wip for me

--- defaults/.config/nvim/lua/plugins/dap.lua
+++ defaults/.config/nvim/lua/plugins/dap.lua
@@ -1,14 +1,14 @@
 local dap = {
 	"mfussenegger/nvim-dap",
   ft = {"lua"},
-	keys = {
-		{ "n", "<F8>", [[:lua require"dap".toggle_breakpoint()<CR>]], { noremap = true } },
-		{ "n", "<F9>", [[:lua require"dap".continue()<CR>]], { noremap = true } },
-		{ "n", "<F10>", [[:lua require"dap".step_over()<CR>]], { noremap = true } },
-		{ "n", "<S-F10>", [[:lua require"dap".step_into()<CR>]], { noremap = true } },
-		{ "n", "<F12>", [[:lua require"dap.ui.widgets".hover()<CR>]], { noremap = true } },
-		{ "n", "<F5>", [[:lua require"osv".launch({port = 8086})<CR>]], { noremap = true } },
-	},
+	-- keys = {
+		-- { "<F8>", [[:lua require"dap".toggle_breakpoint()<CR>]], { noremap = true } },
+		-- { "<F9>", [[:lua require"dap".continue()<CR>]], { noremap = true } },
+		-- { "<F10>", [[:lua require"dap".step_over()<CR>]], { noremap = true } },
+		-- { "<S-F10>", [[:lua require"dap".step_into()<CR>]], { noremap = true } },
+		-- { "<F12>", [[:lua require"dap.ui.widgets".hover()<CR>]], { noremap = true } },
+		-- { "<F5>", [[:lua require"osv".launch({port = 8086})<CR>]], { noremap = true } },
+	-- },
 	config = function()
 		local dap = require("dap")
 		dap.configurations.lua = {
