dd19e503601e902cb949acf6da95585d140224cc
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-01-12 14:25:31 -0500

    chore: formatting

--- defaults/.config/nvim/lua/plugins/lualine.lua
+++ defaults/.config/nvim/lua/plugins/lualine.lua
@@ -1,45 +1,51 @@
 vim.keymap.set("n", "<leader>tr", function()
 	vim.ui.input("Enter Tab name: ", function(text)
-    if text == "" or text == nil then return end
+		if text == "" or text == nil then
+			return
+		end
 		vim.cmd([[LualineRenameTab ]] .. text)
 	end)
 end, { silent = true, desc = "tab_rename_lualine" })
 
 local function cwd()
-  return vim.fn.fnamemodify(vim.fn.getcwd(), ":~")
+	return vim.fn.fnamemodify(vim.fn.getcwd(), ":~")
 end
 
 local function get_lsp_client_name()
-  local clients = vim.lsp.get_clients()
-  if next(clients) == nil then return "" end -- If no LSP enabled, return nothing.
-  local formated_clients = {}
-  for _, client in ipairs(clients) do
-    table.insert(formated_clients, client.name)
-  end
-  return table.concat(formated_clients, ', ')
+	local clients = vim.lsp.get_clients()
+	if next(clients) == nil then
+		return ""
+	end -- If no LSP enabled, return nothing.
+	local formated_clients = {}
+	for _, client in ipairs(clients) do
+		table.insert(formated_clients, client.name)
+	end
+	return table.concat(formated_clients, ", ")
 end
 
 local function plugin_updates()
-  local u = require("lazy.status").updates()
-  if not u or u == nil or u == "" then return false end
-  local nums = string.match(u, "(%d+)")
-  return nums and tonumber(nums) > 15 or false
+	local u = require("lazy.status").updates()
+	if not u or u == nil or u == "" then
+		return false
+	end
+	local nums = string.match(u, "(%d+)")
+	return nums and tonumber(nums) > 15 or false
 end
 
 return {
 	"nvim-lualine/lualine.nvim",
 	event = "VeryLazy",
-  dependencies = {
-    {
-      "nvim-tree/nvim-web-devicons",
-      event = "VeryLazy",
-      cond = vim.fn.exists("$TERMUX_VERSION") < 1,
-    },
-    {
-      "AndreM222/copilot-lualine",
-      event = "VeryLazy",
-    } 
-  },
+	dependencies = {
+		{
+			"nvim-tree/nvim-web-devicons",
+			event = "VeryLazy",
+			cond = vim.fn.exists("$TERMUX_VERSION") < 1,
+		},
+		{
+			"AndreM222/copilot-lualine",
+			event = "VeryLazy",
+		},
+	},
 	opts = function(plugin)
 		local icons = require("utils.utils").icons
 
@@ -59,28 +65,28 @@ return {
 			},
 			sections = {
 				lualine_a = { "mode" },
-        lualine_b = { { "branch" }, { "diff" } },
+				lualine_b = { { "branch" }, { "diff" } },
 				lualine_c = {
-          { cwd, padding = { left = 1, right = 1 } },
-          { "filename", path = 1, separator = "" },
-          { "filetype", icon_only = true, separator = "", padding = { left = 0, right = 0 } },
+					{ cwd, padding = { left = 1, right = 1 } },
+					{ "filename", path = 1, separator = "" },
+					{ "filetype", icon_only = true, separator = "", padding = { left = 0, right = 0 } },
 				},
 				lualine_x = {
 					{
 						require("lazy.status").updates,
-            cond =  plugin_updates,
+						cond = plugin_updates,
 						color = fg("Special"),
 					},
-          'copilot'
+					"copilot",
 				},
 				lualine_y = {
 					{ "diagnostics", sources = { "nvim_diagnostic" } },
-          { get_lsp_client_name },
+					{ get_lsp_client_name },
 				},
-        lualine_z = {
-          { "progress", separator = "", padding = { left = 1, right = 0 } },
-          { "location", padding = { left = 0, right = 1 } },
-        },
+				lualine_z = {
+					{ "progress", separator = "", padding = { left = 1, right = 0 } },
+					{ "location", padding = { left = 0, right = 1 } },
+				},
 			},
 			tabline = {
 				lualine_a = {
@@ -89,7 +95,7 @@ return {
 				lualine_b = {
 					{ "tabs", mode = 2, max_length = vim.o.columns },
 					{
-            -- Pretty awful hack: https://github.com/nvim-lualine/lualine.nvim/issues/921
+						-- Pretty awful hack: https://github.com/nvim-lualine/lualine.nvim/issues/921
 						function()
 							vim.o.showtabline = 1
 							return ""
@@ -114,8 +120,7 @@ return {
 					{ "filetype", icon_only = true },
 					{ "filename", path = 0 },
 				},
-				lualine_x = {
-				},
+				lualine_x = {},
 			},
 			extensions = { "neo-tree", "quickfix", "toggleterm" },
 		}
