488eca7c258b5ec2bbb145a680b58cfd225a432c
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-01-26 20:58:04 +0000

    task: formatting

--- defaults/.config/home-manager/flake.nix
+++ defaults/.config/home-manager/flake.nix
@@ -1,12 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
