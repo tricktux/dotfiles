8289e76eb5666b76b3450b4da86435159c1092e4
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-02-06 19:03:42 +0000

    feat(md): support for sniprun

--- defaults/.config/nvim/lua/plugins/unix.lua
+++ defaults/.config/nvim/lua/plugins/unix.lua
@@ -43,4 +43,25 @@ return {
       require('kitty-scrollback').setup()
     end,
   },
+  {
+    'michaelb/sniprun',
+    cmd = { 'SnipRun' },
+    ft = { "markdown", "org" },
+    build = 'sh ./install.sh',
+    opts = {
+          --# you can combo different display modes as desired and with the 'Ok' or 'Err' suffix
+      --# to filter only sucessful runs (or errored-out runs respectively)
+      display = {
+        "Classic",                    --# display results in the command-line  area
+        "VirtualTextOk",              --# display ok results as virtual text (multiline is shortened)
+        -- "VirtualText",             --# display results as virtual text
+        -- "TempFloatingWindow",      --# display results in a floating window
+        -- "LongTempFloatingWindow",  --# same as above, but only long results. To use with VirtualText[Ok/Err]
+        -- "Terminal",                --# display results in a vertical split
+        "TerminalWithCode",        --# display results and code history in a vertical split
+        -- "NvimNotify",              --# display with the nvim-notify plugin
+        -- "Api"                      --# return output to a programming interface
+      },
+    }
+  }
 }
