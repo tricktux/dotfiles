e0e44cdf37ec2c103b0479c69d4ddd97915ac24e
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-02-08 02:39:00 +0000

    feat: add sudo config

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -132,6 +132,30 @@ in
     };
   };
 
+  security.sudo = {
+    enable = true;
+    extraRules = [{
+      commands = [
+        {
+          command = "${pkgs.systemd}/bin/systemctl suspend";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "${pkgs.systemd}/bin/reboot";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "${pkgs.systemd}/bin/poweroff";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+      groups = [ "wheel" ];
+    }];
+    extraConfig = with pkgs; ''
+      Defaults:reinaldo timestamp_timeout=7200
+    '';
+  };
+
   # Enable automatic login for the user.
   services.getty.autologinUser = "reinaldo";
 
