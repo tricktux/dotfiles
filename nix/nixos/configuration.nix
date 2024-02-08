6d11b6cb1148cf280868ffb5b29f9dabece6a52b
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-02-08 02:23:30 +0000

    fix: group services together

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -124,9 +124,6 @@ in
   # TODO: Set your hostname
   networking.hostName = "surbook";
 
-  # Enable automatic login for the user.
-  services.getty.autologinUser = "reinaldo";
-
   users.users = {
     reinaldo = {
       isNormalUser = true;
@@ -134,6 +131,9 @@ in
       extraGroups = [ "networkmanager" "wheel" "storage" "power" "sys" ];
     };
   };
+
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "reinaldo";
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
