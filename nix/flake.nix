e57d43f636df61c34ea780314a94fbc29b4f2851
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-02-08 02:18:59 +0000

    fix: remove nix fmt command

--- nix/flake.nix
+++ nix/flake.nix
@@ -41,9 +41,6 @@
     # Your custom packages
     # Accessible through 'nix build', 'nix shell', etc
     packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-    # Formatter for your nix files, available through 'nix fmt'
-    # Other options beside 'alejandra' include 'nixpkgs-fmt'
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
     # Your custom packages and modifications, exported as overlays
     overlays = import ./overlays {inherit inputs;};
