7a52fcee43d82db05d97e6d4f2826ee562db26cb
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-02-08 02:11:59 +0000

    task: remove comments

--- nix/flake.nix
+++ nix/flake.nix
@@ -13,9 +13,6 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # TODO: Add any other flake you might need
-    # hardware.url = "github:nixos/nixos-hardware";
-
     # Shameless plug: looking for a way to nixify your themes and make
     # everything match nicely? Try nix-colors!
     # nix-colors.url = "github:misterio77/nix-colors";
--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -133,20 +133,10 @@ in
   # Enable automatic login for the user.
   services.getty.autologinUser = "reinaldo";
 
-  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
-    # FIXME: Replace with your username
     reinaldo = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
-      # initialPassword = "correcthorsebatterystaple";
       isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-      ];
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = ["networkmanager" "wheel"];
+      openssh.authorizedKeys.keys = [ ];
     };
   };
 
