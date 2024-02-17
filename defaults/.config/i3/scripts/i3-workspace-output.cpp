4bc537fa797e53ea0a77a460cdb8a8861777bcfa
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-02-17 21:57:00 +0000

    feat!: major re-write
    - Much better structure, rather a some random ifs
    - Fix issue not detecting eDP, as a valid monitor
    - Use latest cpp niceists

--- defaults/.config/i3/scripts/i3-workspace-output.cpp
+++ defaults/.config/i3/scripts/i3-workspace-output.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <iostream>
 #include <memory>
+#include <optional>
 #include <regex>
 #include <string>
 #include <unordered_map>
@@ -25,9 +26,9 @@
  * @return std::string The last word after space if found, otherwise an empty
  * string.
  */
-std::string get_mon_name(const std::string &text) {
+std::optional<std::string> get_mon_name(const std::string &text) {
   // Define regex pattern to match last word after space
-  std::regex pattern(".*\\s+([A-Za-z0-9]+(-[A-Za-z0-9]+)+)");
+  std::regex pattern(".*\\s+([A-Za-z0-9]+(.*[A-Za-z0-9]+)+)");
 
   // Create a match object and search for the regex pattern in the given text
   std::smatch match;
@@ -36,7 +37,39 @@ std::string get_mon_name(const std::string &text) {
     return match[1];
   }
 
-  return std::string();
+  return std::nullopt;
+}
+
+struct monitor {
+  std::string name;
+  size_t resolution;
+};
+
+struct monitors {
+  std::vector<monitor> m_mon;
+
+  void sort() {
+    std::sort(m_mon.begin(), m_mon.end(), [](const auto &lhs, const auto &rhs) {
+      return lhs.resolution > rhs.resolution;
+    });
+  }
+
+  const std::string &main() const { return m_mon[0].name; }
+  const std::string &secondary() const {
+    return m_mon.size() > 1 ? m_mon[1].name : m_mon[0].name;
+  }
+  const std::string &tertiary() const {
+    const auto nm = m_mon.size();
+    return nm > 2 ? m_mon[2].name : (nm > 1 ? m_mon[1].name : m_mon[0].name);
+  }
+};
+
+std::ostream &operator<<(std::ostream &os, const monitors &m) {
+  os << "monitor: size(" << m.m_mon.size() << ")\n";
+  for (const auto &mon : m.m_mon) {
+    os << "\t" << mon.name << " " << mon.resolution << "\n";
+  }
+  return os;
 }
 
 // Function that returns the monitor resolution
@@ -51,69 +84,54 @@ std::string get_mon_resolution(const std::string &text) {
   return std::string();
 }
 
+std::optional<monitor> builder(const std::string &text) {
+  if (const auto name = get_mon_name(text); name) {
+    if (const auto resolution = get_mon_resolution(text); !resolution.empty()) {
+      return monitor{*name, std::stoul(resolution)};
+    }
+  }
+  return std::nullopt;
+}
+
 int main(int argc, char **argv) {
-  std::string command = "xrandr --listmonitors";
-  std::vector<std::string> lines;
+  constexpr auto command = "xrandr --listmonitors";
 
   // Open a pipe to the command and read its output line by line
-  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.c_str(), "r"),
-                                                pclose);
-
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command, "r"), pclose);
   if (!pipe && !pipe.get()) {
     std::cerr << "Error: failed to run command \"" << command << "\""
               << std::endl;
     return 1;
   }
+
   char buffer[256];
+  std::vector<std::string> lines{};
   while (fgets(buffer, sizeof(buffer), pipe.get()) != nullptr) {
     lines.push_back(buffer);
   }
+
   // Parse the output and find the main and secondary monitors
-  size_t num_mons{lines.size() - 1};
-  if (num_mons < 2) {
-    std::cerr << "Error: not enought monitors: \"" << num_mons << "\""
-              << std::endl;
-    return 1;
+  monitors monitors{};
+  std::cout << "lines: size(" << lines.size() << ")\n";
+  for (const auto &line : lines) {
+    if (const auto mon = builder(line); mon) {
+      std::cout << "\t" << mon->name << " " << mon->resolution << ". orig: '"
+                << line << "'";
+      monitors.m_mon.push_back(std::move(*mon));
+    } else
+      std::cout << "\tNOT A MONITOR: " << line;
   }
 
-  if (num_mons > 3) {
-    std::cerr << "Error: too many monitors: \"" << num_mons << "\""
-              << std::endl;
-    return 2;
-  }
+  monitors.sort();
+  std::cout << monitors;
 
-  bool reserve{false};
-  std::string tm;
-  auto thi_idx = 3;
-  auto sec_idx = 2;
-  if (num_mons == 3) {
-    if (const auto r = get_mon_resolution(lines[2]); r != "3840") {
-      sec_idx = 3;
-      thi_idx = 2;
-    }
-    tm = get_mon_name(lines[thi_idx]);
-    if (tm.empty()) {
-      std::cerr << "Error: failed to parse secondary monitor" << std::endl;
-      return 4;
-    }
-    reserve = true;
-  }
-  auto const pm = get_mon_name(lines[1]);
-  if (pm.empty()) {
-    std::cerr << "Error: failed to parse primary monitor" << std::endl;
-    return 3;
-  }
-
-  auto const sm = get_mon_name(lines[sec_idx]);
-  if (sm.empty()) {
-    std::cerr << "Error: failed to parse secondary monitor" << std::endl;
-    return 5;
-  }
+  const auto &pm{monitors.main()};
+  const auto &sm{monitors.secondary()};
+  const auto &tm{monitors.tertiary()};
 
   const std::unordered_map<uint8_t, std::string> w{
-      {1, pm}, {2, sm}, {3, reserve ? tm : sm},
-      {4, pm}, {5, sm}, {6, pm},
-      {7, sm}, {8, pm}, {9, sm},
+      {1, pm}, {2, sm}, {3, tm}, {4, pm}, {5, sm},
+      {6, pm}, {7, sm}, {8, pm}, {9, sm},
   };
 
   // Loop through the unordered_map while executing the i3 command
