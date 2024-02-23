304d266bc468cc93afd7dc47503c334bd7430386
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-02-23 11:48:07 +0000

    feat(pass): many more modules

--- nix/home-manager/pass.nix
+++ nix/home-manager/pass.nix
@@ -6,7 +6,17 @@
 , ...
 }: {
   home.packages = with pkgs; [
-    pass
+    keepassxc
     rofi-pass
+    pass
+    passExtensions.pass-update
+    # A pass extension that allows to add files to password-store
+    passExtensions.pass-file
+    # A pass extension to check against the Have I been pwned API to see if your passwords are publicly leaked or not
+    passExtensions.pass-checkup
+    # Pass extension that generates memorable passwords
+    passExtensions.pass-genphrase
+    # Pass extension for auditing your password repository
+    passExtensions.pass-audit
   ];
 }
