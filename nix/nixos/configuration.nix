c9a0dae42c7c590504e0bdff8e61800aa673b710
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-03-01 17:23:22 -0500

    feat: cool levels and backups

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -168,6 +168,9 @@
     };
   };
 
+  system.nixos.label = "g${inputs.self.shortRev}";
+  system.copySystemConfiguration = true;
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
 }
