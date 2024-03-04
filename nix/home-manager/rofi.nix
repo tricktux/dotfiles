a9d2e39356f11b22ec6bb1e670ba34ad9283249c
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-03-04 07:35:14 -0500

    wip: drun fix

--- nix/home-manager/rofi.nix
+++ nix/home-manager/rofi.nix
@@ -12,4 +12,16 @@
     url = "https://raw.githubusercontent.com/davatorium/rofi/next/themes/Adapta-Nokto.rasi";
     hash = "sha256-5CQJrVTiklMN5ZwV4jK8rQnHCYJitpcXsfBL1dxUdqQ=";
   };
+
+  # xdg.systemDirs.data = [
+  #   "${config.home.homeDirectory}/.nix-profile/share/applications"  "/usr/share" "/usr/local/share" 
+  # ];
+  # programs.rofi = {
+    # enable = true;
+    # extraConfig = {
+      # import = "${config.home.homeDirectory}/.config/rofi/common.rasi";
+      # import = "${config.home.homeDirectory}/.config/rofi/aero.rasi";
+    # };
+    # theme = "Adapta-Nokto";
+  # };
 }
