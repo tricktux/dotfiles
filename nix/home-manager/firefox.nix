3ee0a6f25347621b5912eb13ee2268205e1e1636
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-03-05 06:21:36 -0500

    feat: add chromium, bookmarks, and search engines

--- nix/home-manager/firefox.nix
+++ nix/home-manager/firefox.nix
@@ -6,16 +6,80 @@
 , ...
 }:
 {
+  # Add chromium just in case something doesn't work
+
+  programs.chromium = {
+    enable = true;
+    commandLineArgs =
+      [
+        "--enable-logging=stderr"
+        "--ignore-gpu-blocklist"
+      ];
+    dictionaries =
+      [
+        pkgs.hunspellDictsChromium.en_US
+      ];
+  };
   programs.firefox = {
     enable = true;
     profiles.reinaldo = {
-      bookmarks = { };
+      search.engines =
+        {
+          "Nix Packages" = {
+            urls = [{
+              template = "https://search.nixos.org/packages";
+              params = [
+                { name = "type"; value = "packages"; }
+                { name = "query"; value = "{searchTerms}"; }
+              ];
+            }];
+
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@np" ];
+          };
+
+          "NixOS Wiki" = {
+            urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
+            iconUpdateURL = "https://nixos.wiki/favicon.png";
+            updateInterval = 24 * 60 * 60 * 1000; # every day
+            definedAliases = [ "@nw" ];
+          };
+
+          "Bing".metaData.hidden = true;
+          "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+        };
+      bookmarks = [
+        {
+          name = "wikipedia";
+          tags = [ "wiki" ];
+          keyword = "wiki";
+          url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
+        }
+        {
+          name = "kernel.org";
+          url = "https://www.kernel.org";
+        }
+        {
+          name = "Nix sites";
+          toolbar = true;
+        }
+        {
+          name = "homepage";
+          url = "https://nixos.org/";
+        }
+        {
+          name = "wiki";
+          tags = [ "wiki" "nix" ];
+          url = "https://nixos.wiki/";
+        }
+      ];
+      # https://github.com/nix-community/nur-combined/blob/master/repos/rycee/pkgs/firefox-addons/generated-firefox-addons.nix
       extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
         ublock-origin
         darkreader
         vimium
+        firenvim
       ];
-      bookmarks = { };
       settings = {
         "browser.disableResetPrompt" = true;
         "browser.download.panel.shown" = true;
