057e65c0052c02ea2321b3430e43ad60f43d7825
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-03-05 11:14:59 -0500

    fix: remove extra inherit

--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -1,7 +1,6 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
 { pkgs ? import <nixpkgs> { } }: rec {
-  inherit (pkgs.llvmPackages_12) stdenv;
   polybar-popup-calendar = pkgs.callPackage ./polybar-popup-calendar.nix { };
   galendae = pkgs.callPackage ./galendae.nix { };
 }
