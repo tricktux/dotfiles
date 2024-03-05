af7b1ad6ebf2f53864a7bddd1e1a937d3b10a2b8
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-03-04 21:15:15 -0500

    fix: not supported options

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -168,9 +168,6 @@
     };
   };
 
-  system.nixos.label = "g${inputs.self.shortRev}";
-  system.copySystemConfiguration = true;
-
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
 }
