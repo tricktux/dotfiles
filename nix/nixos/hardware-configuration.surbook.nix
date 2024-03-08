625e1fd1528c7a0094013a0141657a12a5017e58
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-03-07 22:18:17 -0500

    fix: use latest kernel

--- nix/nixos/hardware-configuration.surbook.nix
+++ nix/nixos/hardware-configuration.surbook.nix
@@ -12,6 +12,9 @@
   # TODO: set hostname
   networking.hostName = "surbook";
 
+  # Use latest
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
