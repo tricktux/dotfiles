3af26f398ac053af491b4db37827d3d9e87b43c8
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-04-21 17:43:18 -0400

    fix: remove cava and added yt stuff

--- nix/home-manager/desktop-environment/music.nix
+++ nix/home-manager/desktop-environment/music.nix
@@ -8,22 +8,11 @@
   home.packages = with pkgs; [
     pavucontrol
     playerctl
+    ncpamixer
+    ytmdl
+    ytfzf
     cmus
   ];
-
-  programs.cava = {
-    enable = true;
-    settings = {
-      general.framerate = 60;
-      input.method = "alsa";
-      smoothing.noise_reduction = 88;
-      color = {
-        background = "'#000000'";
-        foreground = "'#FFFFFF'";
-      };
-    };
-  };
-
 
   # services.easyeffects.enable = true;
   services.playerctld.enable = true;
