69b5a47f31e52f94f02bc922cf61f6ab97af44e0
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-08-12 21:28:39 -0400

    fix(project): change to manual mode
    - It get's a little messed up when files in multiple directories
    - Manual is better

--- defaults/.config/nvim/lua/plugins/telescope.lua
+++ defaults/.config/nvim/lua/plugins/telescope.lua
@@ -189,7 +189,7 @@ function M:config_project()
   require('project_nvim').setup({
     -- Manual mode doesn't automatically change your root directory, so you have
     -- the option to manually do so using `:ProjectRoot` command.
-    manual_mode = false,
+    manual_mode = true,
 
     -- Methods of detecting the root directory. **"lsp"** uses the native neovim
     -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
@@ -549,7 +549,13 @@ return {
   },
   {
     'ahmedkhalf/project.nvim',
-    event = 'VeryLazy',
+    keys = {
+      {
+        '<leader>cr',
+        '<cmd>ProjectRoot<cr>',
+        desc = 'change_root',
+      },
+    },
     config = function()
       M:config_project()
     end,
