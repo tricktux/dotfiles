8a600d8fdcbdfd53eefb3272bec7267dfbbbc61e
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-08-30 17:36:36 -0400

    fix(cscope): do not add default options

--- defaults/.config/nvim/lua/plugins/init.lua
+++ defaults/.config/nvim/lua/plugins/init.lua
@@ -593,39 +593,8 @@ local p = {
     event = 'VeryLazy',
     opts = {
       -- maps related defaults
-      disable_maps = false, -- "true" disables default keymaps
+      disable_maps = true, -- "true" disables default keymaps
       skip_input_prompt = false, -- "true" doesn't ask for input
-      prefix = "<c-g>", -- prefix to trigger maps
-
-      -- cscope related defaults
-      cscope = {
-        -- location of cscope db file
-        db_file = "./cscope.out", -- DB or table of DBs
-        -- NOTE:
-        --   when table of DBs is provided -
-        --   first DB is "primary" and others are "secondary"
-        --   primary DB is used for build and project_rooter
-        -- cscope executable
-        exec = "cscope", -- "cscope" or "gtags-cscope"
-        -- choose your fav picker
-        picker = "quickfix", -- "quickfix", "telescope", "fzf-lua" or "mini-pick"
-        -- size of quickfix window
-        qf_window_size = 5, -- any positive integer
-        -- position of quickfix window
-        qf_window_pos = "bottom", -- "bottom", "right", "left" or "top"
-        -- "true" does not open picker for single result, just JUMP
-        skip_picker_for_single_result = false, -- "false" or "true"
-        -- these args are directly passed to "cscope -f <db_file> <args>"
-        db_build_cmd_args = { "-bqkv" },
-        -- statusline indicator, default is cscope executable
-        statusline_indicator = nil,
-        -- try to locate db_file in parent dir(s)
-        project_rooter = {
-          enable = false, -- "true" or "false"
-          -- change cwd to where db_file is located
-          change_cwd = false, -- "true" or "false"
-        }
-      },
     }
   }
 }
