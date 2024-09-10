73ef4a8a1bf9eed268cd5fb9bd89d40a5746587d
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-09-09 22:27:27 -0400

    fix: remove all visual mode mappings

--- defaults/.config/nvim/lua/plugins/gpt.lua
+++ defaults/.config/nvim/lua/plugins/gpt.lua
@@ -74,27 +74,15 @@ local function mappings()
   vim.keymap.set({"n"}, prefix .. "t", "<cmd>GpChatToggle<cr>", keymapOptions("Toggle Chat"))
   vim.keymap.set({"n"}, prefix .. "f", "<cmd>GpChatFinder<cr>", keymapOptions("Chat Finder"))
 
-  vim.keymap.set("v", prefix .. "c", ":<C-u>'<,'>GpChatNew<cr>", keymapOptions("Visual Chat New"))
-  vim.keymap.set("v", prefix .. "p", ":<C-u>'<,'>GpChatPaste<cr>", keymapOptions("Visual Chat Paste"))
-  vim.keymap.set("v", prefix .. "t", ":<C-u>'<,'>GpChatToggle<cr>", keymapOptions("Visual Toggle Chat"))
 
   vim.keymap.set({ "n" }, prefix .. "<C-x>", "<cmd>GpChatNew split<cr>", keymapOptions("New Chat split"))
   vim.keymap.set({ "n" }, prefix .. "<C-v>", "<cmd>GpChatNew vsplit<cr>", keymapOptions("New Chat vsplit"))
   vim.keymap.set({ "n" }, prefix .. "<C-t>", "<cmd>GpChatNew tabnew<cr>", keymapOptions("New Chat tabnew"))
 
-  vim.keymap.set("v", prefix .. "<C-x>", ":<C-u>'<,'>GpChatNew split<cr>", keymapOptions("Visual Chat New split"))
-  vim.keymap.set("v", prefix .. "<C-v>", ":<C-u>'<,'>GpChatNew vsplit<cr>", keymapOptions("Visual Chat New vsplit"))
-  vim.keymap.set("v", prefix .. "<C-t>", ":<C-u>'<,'>GpChatNew tabnew<cr>", keymapOptions("Visual Chat New tabnew"))
-
   -- Prompt commands
   vim.keymap.set({"n"}, prefix .. "r", "<cmd>GpRewrite<cr>", keymapOptions("Inline Rewrite"))
   vim.keymap.set({"n"}, prefix .. "a", "<cmd>GpAppend<cr>", keymapOptions("Append (after)"))
   vim.keymap.set({"n"}, prefix .. "b", "<cmd>GpPrepend<cr>", keymapOptions("Prepend (before)"))
-
-  vim.keymap.set("v", prefix .. "r", ":<C-u>'<,'>GpRewrite<cr>", keymapOptions("Visual Rewrite"))
-  vim.keymap.set("v", prefix .. "a", ":<C-u>'<,'>GpAppend<cr>", keymapOptions("Visual Append (after)"))
-  vim.keymap.set("v", prefix .. "b", ":<C-u>'<,'>GpPrepend<cr>", keymapOptions("Visual Prepend (before)"))
-  vim.keymap.set("v", prefix .. "i", ":<C-u>'<,'>GpImplement<cr>", keymapOptions("Implement selection"))
 
   vim.keymap.set({"n"}, prefix .. "gp", "<cmd>GpPopup<cr>", keymapOptions("Popup"))
   vim.keymap.set({"n"}, prefix .. "ge", "<cmd>GpEnew<cr>", keymapOptions("GpEnew"))
@@ -102,29 +90,14 @@ local function mappings()
   vim.keymap.set({"n"}, prefix .. "gv", "<cmd>GpVnew<cr>", keymapOptions("GpVnew"))
   vim.keymap.set({"n"}, prefix .. "gt", "<cmd>GpTabnew<cr>", keymapOptions("GpTabnew"))
 
-  vim.keymap.set("v", prefix .. "gp", ":<C-u>'<,'>GpPopup<cr>", keymapOptions("Visual Popup"))
-  vim.keymap.set("v", prefix .. "ge", ":<C-u>'<,'>GpEnew<cr>", keymapOptions("Visual GpEnew"))
-  vim.keymap.set("v", prefix .. "gn", ":<C-u>'<,'>GpNew<cr>", keymapOptions("Visual GpNew"))
-  vim.keymap.set("v", prefix .. "gv", ":<C-u>'<,'>GpVnew<cr>", keymapOptions("Visual GpVnew"))
-  vim.keymap.set("v", prefix .. "gt", ":<C-u>'<,'>GpTabnew<cr>", keymapOptions("Visual GpTabnew"))
-
   vim.keymap.set({"n"}, prefix .. "x", "<cmd>GpContext<cr>", keymapOptions("Toggle Context"))
-  vim.keymap.set("v", prefix .. "x", ":<C-u>'<,'>GpContext<cr>", keymapOptions("Visual Toggle Context"))
-
-  vim.keymap.set({"n", "v", "x"}, prefix .. "s", "<cmd>GpStop<cr>", keymapOptions("Stop"))
-  vim.keymap.set({"n", "v", "x"}, prefix .. "n", "<cmd>GpNextAgent<cr>", keymapOptions("Next Agent"))
 
   -- optional Whisper commands with prefix <C-g>w
   vim.keymap.set({"n"}, prefix .. "ww", "<cmd>GpWhisper<cr>", keymapOptions("Whisper"))
-  vim.keymap.set("v", prefix .. "ww", ":<C-u>'<,'>GpWhisper<cr>", keymapOptions("Visual Whisper"))
 
   vim.keymap.set({"n"}, prefix .. "wr", "<cmd>GpWhisperRewrite<cr>", keymapOptions("Whisper Inline Rewrite"))
   vim.keymap.set({"n"}, prefix .. "wa", "<cmd>GpWhisperAppend<cr>", keymapOptions("Whisper Append (after)"))
   vim.keymap.set({"n"}, prefix .. "wb", "<cmd>GpWhisperPrepend<cr>", keymapOptions("Whisper Prepend (before) "))
-
-  vim.keymap.set("v", prefix .. "wr", ":<C-u>'<,'>GpWhisperRewrite<cr>", keymapOptions("Visual Whisper Rewrite"))
-  vim.keymap.set("v", prefix .. "wa", ":<C-u>'<,'>GpWhisperAppend<cr>", keymapOptions("Visual Whisper Append (after)"))
-  vim.keymap.set("v", prefix .. "wb", ":<C-u>'<,'>GpWhisperPrepend<cr>", keymapOptions("Visual Whisper Prepend (before)"))
 
   vim.keymap.set({"n"}, prefix .. "wp", "<cmd>GpWhisperPopup<cr>", keymapOptions("Whisper Popup"))
   vim.keymap.set({"n"}, prefix .. "we", "<cmd>GpWhisperEnew<cr>", keymapOptions("Whisper Enew"))
@@ -132,11 +105,6 @@ local function mappings()
   vim.keymap.set({"n"}, prefix .. "wv", "<cmd>GpWhisperVnew<cr>", keymapOptions("Whisper Vnew"))
   vim.keymap.set({"n"}, prefix .. "wt", "<cmd>GpWhisperTabnew<cr>", keymapOptions("Whisper Tabnew"))
 
-  vim.keymap.set("v", prefix .. "wp", ":<C-u>'<,'>GpWhisperPopup<cr>", keymapOptions("Visual Whisper Popup"))
-  vim.keymap.set("v", prefix .. "we", ":<C-u>'<,'>GpWhisperEnew<cr>", keymapOptions("Visual Whisper Enew"))
-  vim.keymap.set("v", prefix .. "wn", ":<C-u>'<,'>GpWhisperNew<cr>", keymapOptions("Visual Whisper New"))
-  vim.keymap.set("v", prefix .. "wv", ":<C-u>'<,'>GpWhisperVnew<cr>", keymapOptions("Visual Whisper Vnew"))
-  vim.keymap.set("v", prefix .. "wt", ":<C-u>'<,'>GpWhisperTabnew<cr>", keymapOptions("Visual Whisper Tabnew"))
 end
 
 return {
