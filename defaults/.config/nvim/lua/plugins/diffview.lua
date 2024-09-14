5713062f6675923675f1528bf6846fb333fb9bb4
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-09-14 16:40:15 -0400

    fix: remove extra unnecessary config files
    - That have gone obsolete

--- defaults/.config/nvim/lua/plugins/diffview.lua
+++ defaults/.config/nvim/lua/plugins/diffview.lua
@@ -12,29 +12,6 @@ return {
         layout = 'diff3_mixed',
         disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
       },
-      file_panel = {
-        win_config = {
-          position = 'left', -- One of 'left', 'right', 'top', 'bottom'
-          width = 25, -- Only applies when position is 'left' or 'right'
-          height = 10, -- Only applies when position is 'top' or 'bottom'
-        },
-      },
-      file_history_panel = {
-        win_config = {
-          position = 'bottom',
-          width = 35,
-          height = 16,
-        },
-        log_options = {
-          single_file = {
-            max_count = 512,
-            follow = true,
-          },
-          multi_file = {
-            max_count = 128,
-          },
-        },
-      },
     }
 
     require('diffview').setup(opts)
