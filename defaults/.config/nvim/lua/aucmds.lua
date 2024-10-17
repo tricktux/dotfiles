2daf6152664289bc0cde7518689e48d4333c55d7
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-10-17 05:43:33 -0400

    fix: duplicate
    - we restored the vim version

--- defaults/.config/nvim/lua/aucmds.lua
+++ defaults/.config/nvim/lua/aucmds.lua
@@ -4,7 +4,7 @@ local api = vim.api
 local M = {}
 
 local function set_text_settings()
-  vim.opt_local.wrap = true
+  vim.opt_local.wrap = false
   vim.opt_local.spell = true
   vim.opt.conceallevel = 0
   vim.opt.textwidth = 0
@@ -156,19 +156,6 @@ M.setup = function()
   })
 
   id = api.nvim_create_augroup('Buf', { clear = true })
-  vim.api.nvim_create_autocmd('BufReadPost', {
-    group = id,
-    desc = 'Restore cursor on file open',
-    pattern = '*',
-    callback = function()
-      local mark = vim.api.nvim_buf_get_mark(0, '"')
-      local lcount = vim.api.nvim_buf_line_count(0)
-      if mark[1] > 0 and mark[1] <= lcount then
-        pcall(vim.api.nvim_win_set_cursor, 0, mark)
-      end
-      vim.api.nvim_feedkeys('zz', 'n', true)
-    end,
-  })
   vim.api.nvim_create_autocmd('BufEnter', {
     group = id,
     desc = 'Set winbar',
