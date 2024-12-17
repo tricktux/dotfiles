9bfe2f6974000b2f12b23c95fc7add934eab54f1
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-12-17 05:16:53 -0500

    fix: not so pretty

--- defaults/.config/nvim/lua/plugins/init.lua
+++ defaults/.config/nvim/lua/plugins/init.lua
@@ -253,32 +253,6 @@ local p = {
     end,
   },
   {
-    "sphamba/smear-cursor.nvim",
-    event = 'VeryLazy',
-    cond = os.getenv("KITTY_WINDOW_ID") == nil,
-    opts = {
-      -- Smear cursor color. Defaults to Cursor GUI color if not set.
-      -- Set to "none" to match the text color at the target cursor position.
-      cursor_color = "#d3cdc3",
-
-      -- Background color. Defaults to Normal GUI background color if not set.
-      normal_bg = "#282828",
-
-      -- Smear cursor when switching buffers or windows.
-      smear_between_buffers = true,
-
-      -- Smear cursor when moving within line or to neighbor lines.
-      smear_between_neighbor_lines = true,
-
-      -- Draw the smear in buffer space instead of screen space when scrolling
-      scroll_buffer_space = true,
-
-      -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
-      -- Smears will blend better on all backgrounds.
-      legacy_computing_symbols_support = false,
-    },
-  },
-  {
     'b3nj5m1n/kommentary',
     enabled = vim.fn.has('nvim-0.10') <= 0,
     keys = {
