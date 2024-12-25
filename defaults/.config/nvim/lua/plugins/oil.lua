32e4f80d916932e43bc079a95657630f212c1225
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-12-25 15:25:10 -0500

    fix: remove netrw

--- defaults/.config/nvim/lua/plugins/oil.lua
+++ defaults/.config/nvim/lua/plugins/oil.lua
@@ -65,6 +65,8 @@ return {
     },
   },
   init = function()
+    vim.g.loaded_netrw       = 1
+    vim.g.loaded_netrwPlugin = 1
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'oil',
       callback = function(args)
