50b2a1f98b75af0ca98e2587a211bdc4427ab4b8
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2025-05-03 09:09:21 -0400

    fix: optional logging
    - With this you cannot echo, like on the airplane tasks
    - You can log optionally straight from the command in the json

--- defaults/.config/rofi/scripts/tasks.sh
+++ defaults/.config/rofi/scripts/tasks.sh
@@ -11,7 +11,7 @@ if [[ $# -gt 0 ]]; then
 
   task=$(echo "$tasks" | jq ".[] | select(.name == \"$1\")")
   task_command=$(echo "$task" | jq ".command")
-  coproc ( eval "\"$task_command\" >>/tmp/task.log 2>&1" )
+  coproc ( eval "\"$task_command\"" )
   exit 0
 else
   if [[ $ROFI_RETV -ne 0 ]]; then
