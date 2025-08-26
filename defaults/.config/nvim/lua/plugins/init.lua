f8554e31bae093bbf52482adf43675db816e4dc2
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2025-08-26 06:00:43 -0400

    fix(zen): leave everything alone
    - Don't change a bunch of settings

--- defaults/.config/nvim/lua/plugins/init.lua
+++ defaults/.config/nvim/lua/plugins/init.lua
@@ -307,21 +307,6 @@ local p = {
         backdrop = 1, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
         width = 100, -- width of the Zen window
         height = 1, -- height of the Zen window
-        options = {
-          signcolumn = 'no', -- disable signcolumn
-          number = false, -- disable number column
-          relativenumber = false, -- disable relative numbers
-        },
-      },
-      plugins = {
-        options = {
-          enabled = true,
-          ruler = false, -- disables the ruler text in the cmd line area
-          showcmd = false, -- disables the command in the last line of the screen
-        },
-        twilight = { enabled = false }, -- enable to start Twilight when zen mode opens
-        gitsigns = { enabled = false }, -- disables git signs
-        tmux = { enabled = false }, -- disables the tmux statusline
       },
     },
   },
