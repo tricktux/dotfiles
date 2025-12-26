a74c04a81879ac9e1095df2f9ec891dee23501f1
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2025-12-26 15:51:53 -0500

    fix: move away from the fancy effects
    - Into a minimalistic thing

--- defaults/.config/sway/scripts/lock.sh
+++ defaults/.config/sway/scripts/lock.sh
@@ -8,17 +8,14 @@ sleep 0.1
 
 # Launch swaylock with consistent settings
 exec swaylock \
-    --screenshots \
-    --clock \
-    --indicator \
+    --color 1a0000 \
     --indicator-radius 100 \
-    --indicator-thickness 7 \
-    --effect-blur 7x5 \
-    --effect-vignette 0.5:0.5 \
-    --ring-color bb00cc \
-    --key-hl-color 880033 \
+    --indicator-thickness 8 \
+    --ring-color ff4500 \
+    --key-hl-color ffff00 \
     --line-color 00000000 \
-    --inside-color 00000088 \
+    --inside-color 33000088 \
     --separator-color 00000000 \
-    --grace 2 \
-    --fade-in 0.2
+    --text-color ff6600 \
+    --caps-lock-key-hl-color ffff00 \
+    --show-failed-attempts
