4f877fdc9e45ab7152e28a798021317143963f88
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2026-02-24 05:05:18 -0500

    fix: fold

--- defaults/.config/nvim/lua/plugins/lsp.lua
+++ defaults/.config/nvim/lua/plugins/lsp.lua
@@ -184,6 +184,7 @@ function M.setup_lsp_attach()
         )
       then
         local win = vim.api.nvim_get_current_win()
+        vim.wo[win][0].foldmethod = 'expr'
         vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
       end
 
