672ec5927bbdf64a2736afe4fb5ad1fec4890f85
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2026-03-22 12:17:19 -0400

    fix: enable new ui if available

--- defaults/.config/nvim/lua/options.lua
+++ defaults/.config/nvim/lua/options.lua
@@ -91,9 +91,31 @@ function M:setup()
   vim.opt.redrawtime = 8000
 
   vim.opt.messagesopt = 'hit-enter,history:10000'
-  local extui_ok, extui = pcall(require, 'vim._extui')
+  local extui_ok, extui = pcall(require, 'vim._core.ui2')
   if extui_ok then
-    extui.enable({})
+    extui.enable({
+      enable = true, -- Whether to enable or disable the UI.
+      msg = { -- Options related to the message module.
+        ---@type 'cmd'|'msg' Default message target, either in the
+        ---cmdline or in a separate ephemeral message window.
+        ---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
+        ---or table mapping |ui-messages| kinds and triggers to a target.
+        targets = 'cmd',
+        cmd = { -- Options related to messages in the cmdline window.
+          height = 0.5, -- Maximum height while expanded for messages beyond 'cmdheight'.
+        },
+        dialog = { -- Options related to dialog window.
+          height = 0.5, -- Maximum height.
+        },
+        msg = { -- Options related to msg window.
+          height = 0.5, -- Maximum height.
+          timeout = 888, -- Time a message is visible in the message window.
+        },
+        pager = { -- Options related to message window.
+          height = 1, -- Maximum height.
+        },
+      },
+    })
   end
 
   -- Coming from lazyvim
