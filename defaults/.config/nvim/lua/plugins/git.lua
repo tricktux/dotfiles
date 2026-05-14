5b151e1a199768fcf1aa44f815e5398e88d355c5
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2026-05-14 06:33:51 -0400

    fix(git): reintroduce diffview nvim
    - It's pull request review is too good
    - Plus this is the fork that's being maintained

--- defaults/.config/nvim/lua/plugins/git.lua
+++ defaults/.config/nvim/lua/plugins/git.lua
@@ -1,18 +1,62 @@
+-- Add these functions to your git.lua file, replacing the existing fugitive keymap
 local map = require('mappings')
 
 return {
+  {
+    'dlyongemallo/diffview.nvim',
+    keys = {
+      {
+        map.vcs.prefix .. 'p',
+        function()
+          local base = vim.fn.input('Base branch: ', 'develop')
+          if base ~= '' then
+            vim.cmd('DiffviewOpen ' .. base .. '...HEAD --imply-local')
+          end
+        end,
+        mode = { 'n' },
+        desc = 'diffview: diff against base branch',
+      },
+      {
+        map.vcs.prefix .. 'P',
+        function()
+          local base = vim.fn.input('Base branch: ', 'develop')
+          if base ~= '' then
+            vim.cmd('DiffviewFileHistory --range=' .. base .. '...HEAD --right-only --no-merges')
+          end
+        end,
+        mode = { 'n' },
+        desc = 'diffview: file history vs base branch',
+      },
+      {
+        '<leader>vc',
+        '<cmd>DiffviewFileHistory %<cr>',
+        mode = { 'n' },
+        desc = 'diffview: file history (current file)',
+      },
+    },
+    cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
+    config = function()
+      local opts = {
+        diff_binaries = false,
+        use_icons = false,
+        icons = {
+          folder_closed = '',
+          folder_open = '',
+        },
+        signs = { fold_closed = '>', fold_open = '' },
+        enhanced_diff_hl = true,
+        merge_tool = {
+          layout = 'diff3_mixed',
+          disable_diagnostics = true,
+        },
+        default_args = {
+          DiffviewOpen = { '--imply-local' },
+        },
+      }
+
+      require('diffview').setup(opts)
+    end,
+  },
   'tpope/vim-fugitive',
   cmd = 'Git',
-  keys = {
-    {
-      map.vcs.prefix .. 'p',
-      function()
-        local base = vim.fn.input('Base branch: ', 'develop')
-        if base ~= '' then
-          vim.cmd('Git difftool -y ' .. base .. '..HEAD')
-        end
-      end,
-      desc = 'PR review (diff vs base branch)',
-    },
-  },
 }
